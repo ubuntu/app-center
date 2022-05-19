5b0daec35558faff168de1cac39944d812095d55
Feichtmeier <frederik.feichtmeier@gmail.com>
2022-05-19 10:34:20 +0200

    Update section icons

--- lib/pages/snap_section.dart
+++ lib/pages/snap_section.dart
@@ -27,24 +27,24 @@ enum SnapSection {
 }
 
 Map<SnapSection, IconData> snapSectionToIcon = {
-  SnapSection.art_and_design: YaruIcons.colors,
+  SnapSection.art_and_design: YaruIcons.template,
   SnapSection.books_and_reference: YaruIcons.book,
   SnapSection.development: YaruIcons.wrench,
   SnapSection.devices_and_iot: YaruIcons.chip,
-  SnapSection.education: YaruIcons.book,
+  SnapSection.education: YaruIcons.beaker,
   SnapSection.entertainment: YaruIcons.television,
   SnapSection.featured: YaruIcons.star,
   SnapSection.finance: YaruIcons.calculator,
   SnapSection.games: YaruIcons.games,
   SnapSection.health_and_fitness: YaruIcons.health,
   SnapSection.music_and_audio: YaruIcons.headphones,
-  SnapSection.news_and_weather: YaruIcons.text_editor,
+  SnapSection.news_and_weather: YaruIcons.weather_storm,
   SnapSection.personalisation: YaruIcons.desktop_appearance,
   SnapSection.photo_and_video: YaruIcons.camera_photo,
   SnapSection.productivity: YaruIcons.clock,
   SnapSection.science: YaruIcons.beaker,
   SnapSection.security: YaruIcons.shield,
   SnapSection.server_and_cloud: YaruIcons.computer,
-  SnapSection.social: YaruIcons.users,
+  SnapSection.social: YaruIcons.subtitles,
   SnapSection.utilities: YaruIcons.utilities,
 };
