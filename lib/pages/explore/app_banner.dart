7741602b9b6ee0d4c623f0d01c06d433a9648407
Feichtmeier <frederik.feichtmeier@gmail.com>
2022-05-25 10:53:38 +0200

    AppBanner: extract card

--- lib/pages/explore/app_banner.dart
+++ lib/pages/explore/app_banner.dart
@@ -19,10 +19,6 @@ class AppBanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final borderRadius = BorderRadius.circular(10);
-    Icon fallBackIcon = Icon(
-      YaruIcons.package_snap,
-      size: 65,
-    );
     int? iconIndex;
     for (var i = 0; i < snap.media.length; i++) {
       if (snap.media[i].type == 'icon') {
@@ -36,72 +32,37 @@ class AppBanner extends StatelessWidget {
     return InkWell(
       onTap: onTap,
       borderRadius: borderRadius,
-      child: surfaceTint
+      child: surfaceTint && iconIndex != null
           ? FutureBuilder<Color>(
-              future: iconIndex != null
-                  ? getSurfaceTintColor(NetworkImage(snap.media[iconIndex].url))
-                  : Future(() => Theme.of(context).primaryColor),
-              builder: (context, snapshot) => Card(
-                surfaceTintColor: snapshot.data == null
-                    ? light
-                        ? YaruColors.warmGrey.shade900
-                        : Theme.of(context).colorScheme.onBackground
-                    : snapshot.data,
-                elevation: 4,
-                shape: RoundedRectangleBorder(
+              future:
+                  getSurfaceTintColor(NetworkImage(snap.media[iconIndex].url)),
+              builder: (context, snapshot) => _Card(
                   borderRadius: borderRadius,
-                ),
-                child: Align(
-                  alignment: Alignment.center,
-                  child: ListTile(
-                    subtitle:
-                        Text(snap.summary, overflow: TextOverflow.ellipsis),
-                    title: Text(
-                      snap.title,
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    leading: SizedBox(
-                      width: 60,
-                      child: iconIndex != null
-                          ? Image.network(
-                              snap.media[iconIndex].url,
-                              fit: BoxFit.fitHeight,
-                              filterQuality: FilterQuality.medium,
-                            )
-                          : fallBackIcon,
-                    ),
-                  ),
-                ),
-              ),
+                  color: snapshot.data ?? Colors.transparent,
+                  title: snap.title,
+                  summary: snap.summary,
+                  elevation: 4,
+                  icon: Image.network(
+                    snap.media[iconIndex!].url,
+                    fit: BoxFit.fitHeight,
+                    filterQuality: FilterQuality.medium,
+                  )),
             )
-          : Card(
-              surfaceTintColor: light
+          : _Card(
+              borderRadius: borderRadius,
+              color: light
                   ? YaruColors.warmGrey.shade900
                   : Theme.of(context).colorScheme.onBackground,
               elevation: light ? 2 : 1,
-              shape: RoundedRectangleBorder(
-                borderRadius: borderRadius,
-              ),
-              child: Align(
-                alignment: Alignment.center,
-                child: ListTile(
-                  subtitle: Text(snap.summary, overflow: TextOverflow.ellipsis),
-                  title: Text(
-                    snap.title,
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  leading: SizedBox(
-                    width: 60,
-                    child: iconIndex != null
-                        ? Image.network(
-                            snap.media[iconIndex].url,
-                            fit: BoxFit.fitHeight,
-                            filterQuality: FilterQuality.medium,
-                          )
-                        : fallBackIcon,
-                  ),
-                ),
-              ),
+              icon: iconIndex != null
+                  ? Image.network(
+                      snap.media[iconIndex].url,
+                      fit: BoxFit.fitHeight,
+                      filterQuality: FilterQuality.medium,
+                    )
+                  : _FallbackIcon(),
+              title: snap.title,
+              summary: snap.summary,
             ),
     );
   }
@@ -111,5 +72,63 @@ class AppBanner extends StatelessWidget {
       imageProvider,
     );
     return paletteGenerator.paletteColors.first.color;
+  }
+}
+
+class _FallbackIcon extends StatelessWidget {
+  const _FallbackIcon({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      YaruIcons.package_snap,
+      size: 65,
+    );
+  }
+}
+
+class _Card extends StatelessWidget {
+  const _Card({
+    Key? key,
+    required this.color,
+    required this.title,
+    required this.summary,
+    required this.elevation,
+    required this.icon,
+    required this.borderRadius,
+  }) : super(key: key);
+
+  final Color color;
+  final String title;
+  final String summary;
+  final double elevation;
+  final Widget icon;
+  final BorderRadius borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      surfaceTintColor: color,
+      elevation: elevation,
+      shape: RoundedRectangleBorder(
+        borderRadius: borderRadius,
+      ),
+      child: Align(
+        alignment: Alignment.center,
+        child: ListTile(
+          subtitle: Text(summary, overflow: TextOverflow.ellipsis),
+          title: Text(
+            title,
+            style: TextStyle(fontSize: 20),
+          ),
+          leading: SizedBox(
+            width: 60,
+            child: icon,
+          ),
+        ),
+      ),
+    );
   }
 }
