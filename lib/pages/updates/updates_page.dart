163f6fb281ff8b4db60e79952ab925d70d4e99ee
Feichtmeier <frederik.feichtmeier@gmail.com>
2022-06-01 20:43:51 +0200

    UpdatesPage: remove unused imports

--- lib/pages/updates/updates_page.dart
+++ lib/pages/updates/updates_page.dart
@@ -1,13 +1,6 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:snapd/snapd.dart';
 import 'package:software/l10n/l10n.dart';
-import 'package:software/pages/common/apps_model.dart';
-import 'package:software/pages/common/snap_model.dart';
-import 'package:software/pages/common/snap_tile.dart';
 import 'package:software/pages/updates/snap_updates_page.dart';
-import 'package:ubuntu_service/ubuntu_service.dart';
 import 'package:yaru_icons/yaru_icons.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
 
