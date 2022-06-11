19a44e39b072678b69be6323a7a23033d2ac5c47
Feichtmeier <frederik.feichtmeier@gmail.com>
2022-06-11 17:59:14 +0200

    Fix imports

--- lib/store_app/common/snap_channel_expandable.dart
+++ lib/store_app/common/snap_channel_expandable.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:provider/provider.dart';
 import 'package:software/l10n/l10n.dart';
 import 'package:software/store_app/common/snap_model.dart';
