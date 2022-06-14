d07669e23d5a59433c52badf0ffc550ee0fc5b66
Feichtmeier <frederik.feichtmeier@gmail.com>
2022-06-14 10:16:44 +0200

    SnapDialog content: use always the same width to avoid size jumping

--- lib/store_app/common/snap_content.dart
+++ lib/store_app/common/snap_content.dart
@@ -21,93 +21,96 @@ class SnapContent extends StatelessWidget {
             .where((snapMedia) => snapMedia.type == 'screenshot')
             .toList()
         : [];
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        if (media.isNotEmpty)
-          YaruExpandable(
-            expandIcon: const Icon(YaruIcons.pan_end),
-            isExpanded: true,
-            header: const SizedBox(
-              width: 395,
-              child: Divider(),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: YaruCarousel(
-                nextIcon: const Icon(YaruIcons.go_next),
-                previousIcon: const Icon(YaruIcons.go_previous),
-                navigationControls: media.length > 1,
-                viewportFraction: 1,
-                height: 250,
-                children: [
-                  for (final image in media)
-                    InkWell(
-                      borderRadius: BorderRadius.circular(10),
-                      onTap: () => showDialog(
-                        context: context,
-                        builder: (context) => SimpleDialog(
-                          children: [
-                            InkWell(
-                              onTap: () => Navigator.of(context).pop(),
-                              child: SafeImage(
-                                url: image.url,
-                                fit: BoxFit.contain,
-                                filterQuality: FilterQuality.medium,
-                              ),
-                            )
-                          ],
+    return SizedBox(
+      width: 430,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (media.isNotEmpty)
+            YaruExpandable(
+              expandIcon: const Icon(YaruIcons.pan_end),
+              isExpanded: true,
+              header: const SizedBox(
+                width: 390,
+                child: Divider(),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: YaruCarousel(
+                  nextIcon: const Icon(YaruIcons.go_next),
+                  previousIcon: const Icon(YaruIcons.go_previous),
+                  navigationControls: media.length > 1,
+                  viewportFraction: 1,
+                  height: 250,
+                  children: [
+                    for (final image in media)
+                      InkWell(
+                        borderRadius: BorderRadius.circular(10),
+                        onTap: () => showDialog(
+                          context: context,
+                          builder: (context) => SimpleDialog(
+                            children: [
+                              InkWell(
+                                onTap: () => Navigator.of(context).pop(),
+                                child: SafeImage(
+                                  url: image.url,
+                                  fit: BoxFit.contain,
+                                  filterQuality: FilterQuality.medium,
+                                ),
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                      child: SafeImage(
-                        url: image.url,
-                      ),
-                    )
+                        child: SafeImage(
+                          url: image.url,
+                        ),
+                      )
+                  ],
+                ),
+              ),
+            ),
+          const SizedBox(
+            height: 10,
+          ),
+          if (model.contact != null && model.publisher != null)
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  if (model.website != null)
+                    Link(url: model.website!, linkText: context.l10n.website),
+                  Link(
+                    url: model.contact!,
+                    linkText:
+                        '${context.l10n.contact} ${model.publisher!.displayName}',
+                  ),
                 ],
               ),
             ),
+          const SizedBox(
+            height: 10,
           ),
-        const SizedBox(
-          height: 10,
-        ),
-        if (model.contact != null && model.publisher != null)
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                if (model.website != null)
-                  Link(url: model.website!, linkText: context.l10n.website),
-                Link(
-                  url: model.contact!,
-                  linkText:
-                      '${context.l10n.contact} ${model.publisher!.displayName}',
-                ),
-              ],
+          if (model.description != null)
+            YaruExpandable(
+              header: Text(
+                context.l10n.description,
+              ),
+              expandIcon: const Icon(YaruIcons.pan_end),
+              child: Text(
+                model.description!,
+                overflow: TextOverflow.fade,
+              ),
             ),
+          const SizedBox(
+            height: 10,
           ),
-        const SizedBox(
-          height: 10,
-        ),
-        if (model.description != null)
-          YaruExpandable(
-            header: Text(
-              context.l10n.description,
-            ),
-            expandIcon: const Icon(YaruIcons.pan_end),
-            child: Text(
-              model.description!,
-              overflow: TextOverflow.fade,
-            ),
+          const Divider(
+            height: 10,
           ),
-        const SizedBox(
-          height: 10,
-        ),
-        const Divider(
-          height: 10,
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
