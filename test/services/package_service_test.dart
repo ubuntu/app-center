ea7393fd3d09be5441b8e0c46b48811e3042ac3e
Olivier <olivier.tilloy@canonical.com>
2022-10-11 10:07:42 +0200

    Initial tests (#378)
    * Initial tests for PackageService.
    * Initial widget tests for StoreApp.
    * Do not version pubspec.lock.
    * Pin the dependency on yaru_widgets because https://github.com/ubuntu/yaru_widgets.dart/commit/d2183b26e1be1b6dac36ecac71b0783ead26ec10 breaks.

---
+++ test/services/package_service_test.dart
@@ -0,0 +1,383 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:desktop_notifications/desktop_notifications.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:packagekit/packagekit.dart';
+import 'package:path/path.dart' as p;
+import 'package:software/package_state.dart';
+import 'package:software/services/package_service.dart';
+import 'package:software/updates_state.dart';
+import 'package:ubuntu_service/ubuntu_service.dart';
+
+import 'package_service_test.mocks.dart';
+
+@GenerateMocks([
+  Notification,
+  NotificationsClient,
+  PackageKitClient,
+  PackageKitTransaction,
+])
+void main() {
+  late MockPackageKitClient mockPKClient;
+  late MockNotificationsClient mockNotificationsClient;
+
+  const firefoxPackageId =
+      PackageKitPackageId(name: 'firefox', version: '105.0.2');
+
+  Future<void> emitFinishedEvent(controller) {
+    controller.add(
+      const PackageKitFinishedEvent(
+        exit: PackageKitExit.success,
+        runtime: 10,
+      ),
+    );
+    return Future.value();
+  }
+
+  MockPackageKitTransaction createMockTransaction() {
+    final transaction = MockPackageKitTransaction();
+    final controller = StreamController<PackageKitEvent>.broadcast();
+    when(transaction.events).thenAnswer((_) => controller.stream);
+
+    when(transaction.getRepositoryList())
+        .thenAnswer((_) => emitFinishedEvent(controller));
+
+    when(transaction.refreshCache())
+        .thenAnswer((_) => emitFinishedEvent(controller));
+
+    when(transaction.getUpdates(filter: {}))
+        .thenAnswer((_) => emitFinishedEvent(controller));
+
+    when(transaction.getPackages(filter: {PackageKitFilter.installed}))
+        .thenAnswer((_) => emitFinishedEvent(controller));
+
+    when(
+      transaction.getPackages(
+        filter: {
+          PackageKitFilter.installed,
+          PackageKitFilter.gui,
+          PackageKitFilter.newest,
+          PackageKitFilter.application,
+          PackageKitFilter.notDevelopment,
+          PackageKitFilter.notSource,
+          PackageKitFilter.visible,
+        },
+      ),
+    ).thenAnswer((_) => emitFinishedEvent(controller));
+
+    when(transaction.getDetails([firefoxPackageId])).thenAnswer((_) {
+      controller.add(
+        PackageKitDetailsEvent(
+          packageId: firefoxPackageId,
+          summary: 'a summary',
+          url: 'https://example.org/',
+          license: 'a license',
+          size: 43008,
+          description: 'a description',
+          group: PackageKitGroup.internet,
+        ),
+      );
+      return emitFinishedEvent(controller);
+    });
+
+    when(transaction.searchNames(['fire'], filter: {})).thenAnswer((_) {
+      controller.add(
+        const PackageKitPackageEvent(
+          info: PackageKitInfo.installed,
+          packageId: firefoxPackageId,
+          summary: 'a fox',
+        ),
+      );
+      controller.add(
+        const PackageKitPackageEvent(
+          info: PackageKitInfo.installed,
+          packageId: PackageKitPackageId(name: 'firejail', version: '0.9.66-2'),
+          summary: 'a jail',
+        ),
+      );
+      return emitFinishedEvent(controller);
+    });
+
+    when(transaction.setRepositoryEnabled(any, any))
+        .thenAnswer((_) => emitFinishedEvent(controller));
+
+    when(transaction.getDetailsLocal(any)).thenAnswer((_) {
+      controller.add(
+        PackageKitDetailsEvent(
+          packageId: firefoxPackageId,
+          group: PackageKitGroup.internet,
+          summary: 'a fox',
+          description: 'a fire fox',
+          url: 'https://example.org/',
+          license: 'a license',
+          size: 43008,
+        ),
+      );
+      return emitFinishedEvent(controller);
+    });
+
+    when(
+      transaction
+          .searchNames(['firefox'], filter: {PackageKitFilter.installed}),
+    ).thenAnswer((_) {
+      controller.add(
+        const PackageKitPackageEvent(
+          info: PackageKitInfo.installed,
+          packageId: firefoxPackageId,
+          summary: 'a fox',
+        ),
+      );
+      return emitFinishedEvent(controller);
+    });
+
+    when(transaction.installPackages([firefoxPackageId])).thenAnswer((_) {
+      controller.add(
+        const PackageKitPackageEvent(
+          info: PackageKitInfo.installing,
+          packageId: firefoxPackageId,
+          summary: 'a fox',
+        ),
+      );
+      controller.add(
+        const PackageKitItemProgressEvent(
+          packageId: firefoxPackageId,
+          status: PackageKitStatus.install,
+          percentage: 33,
+        ),
+      );
+      controller.add(
+        const PackageKitItemProgressEvent(
+          packageId: firefoxPackageId,
+          status: PackageKitStatus.install,
+          percentage: 67,
+        ),
+      );
+      controller.add(
+        const PackageKitItemProgressEvent(
+          packageId: firefoxPackageId,
+          status: PackageKitStatus.install,
+          percentage: 100,
+        ),
+      );
+      return emitFinishedEvent(controller);
+    });
+
+    when(transaction.removePackages([firefoxPackageId])).thenAnswer((_) {
+      controller.add(
+        const PackageKitPackageEvent(
+          info: PackageKitInfo.removing,
+          packageId: firefoxPackageId,
+          summary: 'a fox',
+        ),
+      );
+      controller.add(
+        const PackageKitItemProgressEvent(
+          packageId: firefoxPackageId,
+          status: PackageKitStatus.remove,
+          percentage: 27,
+        ),
+      );
+      controller.add(
+        const PackageKitItemProgressEvent(
+          packageId: firefoxPackageId,
+          status: PackageKitStatus.remove,
+          percentage: 72,
+        ),
+      );
+      controller.add(
+        const PackageKitItemProgressEvent(
+          packageId: firefoxPackageId,
+          status: PackageKitStatus.remove,
+          percentage: 100,
+        ),
+      );
+      return emitFinishedEvent(controller);
+    });
+
+    return transaction;
+  }
+
+  setUp(() {
+    mockPKClient = MockPackageKitClient();
+    registerMockService<PackageKitClient>(mockPKClient);
+    when(mockPKClient.createTransaction())
+        .thenAnswer((_) => Future.value(createMockTransaction()));
+
+    mockNotificationsClient = MockNotificationsClient();
+    registerMockService<NotificationsClient>(mockNotificationsClient);
+    when(
+      mockNotificationsClient.notify(
+        any,
+        body: anyNamed('body'),
+        appName: anyNamed('appName'),
+        appIcon: anyNamed('appIcon'),
+        hints: anyNamed('hints'),
+      ),
+    ).thenAnswer((_) => Future.value(MockNotification()));
+  });
+
+  tearDown(() {
+    unregisterMockService<NotificationsClient>();
+    unregisterMockService<PackageKitClient>();
+  });
+
+  test('instantiate service', () {
+    // ignore: unused_local_variable
+    final service = PackageService();
+
+    verify(mockPKClient.connect()).called(1);
+  });
+
+  test('init', () async {
+    final service = PackageService();
+
+    expectLater(
+      service.packageState,
+      emitsInOrder(
+        [PackageState.processing, PackageState.ready],
+      ),
+    );
+
+    await service.init();
+  });
+
+  test('no updates', () async {
+    final service = PackageService();
+    expect(service.updates, isEmpty);
+
+    expectLater(
+      service.updatesState,
+      emitsInOrder(
+        [UpdatesState.checkingForUpdates, UpdatesState.noUpdates],
+      ),
+    );
+
+    await service.refreshUpdates();
+
+    expect(service.lastUpdatesState, UpdatesState.noUpdates);
+  });
+
+  test('get details', () async {
+    final service = PackageService();
+
+    expectLater(service.summary, emits('a summary'));
+    expectLater(service.url, emits('https://example.org/'));
+    expectLater(service.license, emits('a license'));
+    expectLater(service.size, emits('42.00 KB'));
+    expectLater(service.description, emits('a description'));
+
+    await service.getDetails(packageId: firefoxPackageId);
+  });
+
+  test('find package ids', () async {
+    final service = PackageService();
+
+    final results = await service.findPackageKitPackageIds(searchQuery: 'fire');
+    expect(results.length, 2);
+    expect(results[0], firefoxPackageId);
+    expect(
+      results[1],
+      const PackageKitPackageId(name: 'firejail', version: '0.9.66-2'),
+    );
+  });
+
+  test('toggle repo', () async {
+    final service = PackageService();
+
+    expectLater(service.reposChanged, emitsInOrder([true, true]));
+
+    await service.toggleRepo(id: 'myrepository', value: true);
+    await service.toggleRepo(id: 'myotherrepository', value: false);
+  });
+
+  test('send update notification', () async {
+    final service = PackageService();
+
+    const body = 'ho ho ho';
+    expect(service.lastUpdatesState, isNull);
+    service.sendUpdateNotification(updatesAvailable: body);
+    verifyNever(
+      mockNotificationsClient.notify(
+        any,
+        body: body,
+        appName: anyNamed('appName'),
+        appIcon: anyNamed('appIcon'),
+        hints: anyNamed('hints'),
+      ),
+    );
+
+    service.lastUpdatesState = UpdatesState.readyToUpdate;
+    service.sendUpdateNotification(updatesAvailable: body);
+    verify(
+      mockNotificationsClient.notify(
+        any,
+        body: body,
+        appName: anyNamed('appName'),
+        appIcon: anyNamed('appIcon'),
+        hints: anyNamed('hints'),
+      ),
+    ).called(1);
+  });
+
+  test('get details about local package', () async {
+    final service = PackageService();
+
+    final tempFile = File(p.join(Directory.systemTemp.path, 'tempfile.deb'));
+    await tempFile.openSync(mode: FileMode.write).close();
+
+    expectLater(service.processedId, emits(firefoxPackageId));
+    expectLater(service.summary, emits('a fox'));
+    expectLater(service.url, emits('https://example.org/'));
+    expectLater(service.license, emits('a license'));
+    expectLater(service.size, emits('42.00 KB'));
+    expectLater(service.group, emits(PackageKitGroup.internet));
+    expectLater(service.description, emits('a fire fox'));
+
+    await service.getDetailsAboutLocalPackage(path: tempFile.path);
+
+    await tempFile.delete();
+  });
+
+  test('install package', () async {
+    final service = PackageService();
+
+    expectLater(
+      service.packageState,
+      emitsInOrder(
+        [
+          PackageState.processing,
+          PackageState.processing,
+          PackageState.ready,
+          PackageState.ready,
+        ],
+      ),
+    );
+    expectLater(service.info, emitsInOrder([PackageKitInfo.installing]));
+    expectLater(service.packagePercentage, emitsInOrder([33, 67, 100]));
+
+    await service.install(packageId: firefoxPackageId);
+  });
+
+  test('remove package', () async {
+    final service = PackageService();
+
+    expectLater(
+      service.packageState,
+      emitsInOrder(
+        [
+          PackageState.processing,
+          PackageState.processing,
+          PackageState.ready,
+          PackageState.ready,
+        ],
+      ),
+    );
+    expectLater(service.info, emitsInOrder([PackageKitInfo.removing]));
+    expectLater(service.packagePercentage, emitsInOrder([73, 28, 0]));
+
+    await service.remove(packageId: firefoxPackageId);
+  });
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,14 +1,65 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:software/services/package_service.dart';
+import 'package:software/services/snap_service.dart';
+import 'package:software/store_app/store_app.dart';
+import 'package:software/updates_state.dart';
+import 'package:ubuntu_service/ubuntu_service.dart';
 
+import 'widget_test.mocks.dart';
+
+@GenerateMocks(
+  [
+    Connectivity,
+    PackageService,
+    SnapService,
+  ],
+)
 void main() {
-  testWidgets('Find MaterialApp', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      const MaterialApp(
-        home: Scaffold(),
-      ),
-    );
+  testWidgets('Software app', (tester) async {
+    final connectivityMock = MockConnectivity();
+    registerMockService<Connectivity>(connectivityMock);
+    final connectivityChangedController =
+        StreamController<ConnectivityResult>.broadcast();
+    when(connectivityMock.onConnectivityChanged)
+        .thenAnswer((_) => connectivityChangedController.stream);
+    when(connectivityMock.checkConnectivity())
+        .thenAnswer((_) => Future.value(ConnectivityResult.none));
 
-    expect(find.byType(Scaffold), findsNWidgets(1));
+    final packageServiceMock = MockPackageService();
+    registerMockService<PackageService>(packageServiceMock);
+    when(packageServiceMock.updates).thenReturn([]);
+    final updatesChangedController = StreamController<bool>.broadcast();
+    when(packageServiceMock.updatesChanged)
+        .thenAnswer((_) => updatesChangedController.stream);
+    final updatesStateController = StreamController<UpdatesState>.broadcast();
+    when(packageServiceMock.updatesState)
+        .thenAnswer((_) => updatesStateController.stream);
+
+    final snapServiceMock = MockSnapService();
+    registerMockService<SnapService>(snapServiceMock);
+    when(snapServiceMock.snapChanges).thenReturn({});
+    final snapChangesInsertedController = StreamController<bool>.broadcast();
+    when(snapServiceMock.snapChangesInserted)
+        .thenAnswer((_) => snapChangesInsertedController.stream);
+    when(
+      snapServiceMock.findSnapsBySection(
+        sectionName: anyNamed('sectionName'),
+      ),
+    ).thenAnswer((_) => Future.value([]));
+
+    await tester.pumpWidget(StoreApp.create());
+
+    final materialApp = find.byType(MaterialApp);
+    expect(materialApp, findsOneWidget);
+    expect(
+      (materialApp.evaluate().single.widget as MaterialApp).title,
+      'Ubuntu Software App',
+    );
   });
 }
