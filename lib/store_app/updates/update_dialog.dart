0560ae61f8cc5a9d5dcf4f98e02ff05f381a3505
Feichtmeier <frederik.feichtmeier@gmail.com>
2022-10-11 12:01:40 +0200

    improve layout

--- lib/store_app/updates/update_dialog.dart
+++ lib/store_app/updates/update_dialog.dart
@@ -68,113 +68,142 @@ class _UpdateDialogState extends State<UpdateDialog> {
   @override
   Widget build(BuildContext context) {
     final model = context.watch<PackageModel>();
+    const headerStyle = TextStyle(fontWeight: FontWeight.w500);
+    if (model.packageState != PackageState.ready || model.changelog.isEmpty) {
+      return const AlertDialog(
+        content: Padding(
+          padding: EdgeInsets.only(bottom: 40, top: 40),
+          child: YaruCircularProgressIndicator(),
+        ),
+      );
+    }
     return AlertDialog(
       title: YaruDialogTitle(
+        titleWidget: const Icon(YaruIcons.debian),
         title: model.packageState != PackageState.ready ? null : widget.id.name,
         closeIconData: YaruIcons.window_close,
       ),
       titlePadding: EdgeInsets.zero,
       contentPadding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
       scrollable: true,
-      content: model.packageState != PackageState.ready ||
-              model.changelog.isEmpty
-          ? const Center(
-              child: Padding(
-                padding: EdgeInsets.only(bottom: 40),
-                child: YaruCircularProgressIndicator(),
+      content: SizedBox(
+        width: 400,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            YaruExpandable(
+              header: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  context.l10n.changelog,
+                  style: headerStyle,
+                ),
               ),
-            )
-          : SizedBox(
-              width: 400,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              expandIcon: const Icon(YaruIcons.pan_end),
+              isExpanded: true,
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(
+                  maxHeight: 150,
+                  minHeight: 150,
+                ),
+                // height: 250,
+                child: Markdown(
+                  data: model.changelog.length > 4000
+                      ? '${model.changelog.substring(0, 4000)}\n\n ... ${context.l10n.changelogTooLong} ${model.url}'
+                      : model.changelog,
+                  shrinkWrap: true,
+                  selectable: true,
+                  onTapLink: (text, href, title) =>
+                      href != null ? launchUrl(Uri.parse(href)) : null,
+                  padding: const EdgeInsets.only(left: 8, right: 8),
+                ),
+              ),
+            ),
+            YaruTile(
+              title: Text(
+                context.l10n.version,
+                style: headerStyle,
+              ),
+              trailing: Text(widget.id.version),
+            ),
+            YaruTile(
+              title: Text(
+                context.l10n.size,
+                style: headerStyle,
+              ),
+              trailing: Text(model.size),
+            ),
+            YaruTile(
+              title: Text(
+                context.l10n.architecture,
+                style: headerStyle,
+              ),
+              trailing: Text(widget.id.arch),
+            ),
+            YaruTile(
+              title: Text(
+                context.l10n.source,
+                style: headerStyle,
+              ),
+              trailing: Text(widget.id.data),
+            ),
+            YaruTile(
+              title: Text(
+                context.l10n.license,
+                style: headerStyle,
+              ),
+              trailing: Text(model.license),
+            ),
+            YaruTile(
+              title: Text(
+                context.l10n.website,
+                style: headerStyle,
+              ),
+              trailing: IconButton(
+                splashRadius: 20,
+                tooltip: model.url,
+                onPressed: () => launchUrl(Uri.parse(model.url)),
+                icon: Icon(
+                  YaruIcons.external_link,
+                  color: Theme.of(context).colorScheme.onSurface,
+                  size: 20,
+                ),
+              ),
+              enabled: true,
+            ),
+            YaruTile(
+              title: Text(
+                context.l10n.issued,
+                style: headerStyle,
+              ),
+              trailing: Text(model.issued),
+            ),
+            YaruExpandable(
+              header: Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text(
+                  context.l10n.description,
+                  style: headerStyle,
+                ),
+              ),
+              isExpanded: false,
+              expandIcon: const Icon(YaruIcons.pan_end),
+              child: Row(
                 children: [
-                  YaruTile(
-                    title: Text(context.l10n.version),
-                    trailing: Text(widget.id.version),
-                  ),
-                  YaruTile(
-                    title: Text(context.l10n.size),
-                    trailing: Text(model.size),
-                  ),
-                  YaruExpandable(
-                    header: Padding(
+                  Expanded(
+                    child: Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Text(context.l10n.changelog),
-                    ),
-                    expandIcon: const Icon(YaruIcons.pan_end),
-                    isExpanded: true,
-                    child: ConstrainedBox(
-                      constraints: const BoxConstraints(
-                        maxHeight: 150,
-                        minHeight: 150,
+                      child: Text(
+                        model.description,
                       ),
-                      // height: 250,
-                      child: Markdown(
-                        data: model.changelog.length > 4000
-                            ? '${model.changelog.substring(0, 4000)}\n\n ... ${context.l10n.changelogTooLong} ${model.url}'
-                            : model.changelog,
-                        shrinkWrap: true,
-                        selectable: true,
-                        onTapLink: (text, href, title) =>
-                            href != null ? launchUrl(Uri.parse(href)) : null,
-                        padding: const EdgeInsets.only(left: 8, right: 8),
-                      ),
-                    ),
-                  ),
-                  YaruTile(
-                    title: Text(context.l10n.architecture),
-                    trailing: Text(widget.id.arch),
-                  ),
-                  YaruTile(
-                    title: Text(context.l10n.source),
-                    trailing: Text(widget.id.data),
-                  ),
-                  YaruTile(
-                    title: Text(context.l10n.license),
-                    trailing: Text(model.license),
-                  ),
-                  YaruTile(
-                    title: Text(context.l10n.website),
-                    trailing: IconButton(
-                      splashRadius: 20,
-                      tooltip: model.url,
-                      onPressed: () => launchUrl(Uri.parse(model.url)),
-                      icon: Icon(
-                        YaruIcons.external_link,
-                        color: Theme.of(context).colorScheme.onSurface,
-                        size: 20,
-                      ),
-                    ),
-                    enabled: true,
-                  ),
-                  YaruTile(
-                    title: Text(context.l10n.issued),
-                    trailing: Text(model.issued),
-                  ),
-                  YaruExpandable(
-                    header: Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: Text(context.l10n.description),
-                    ),
-                    isExpanded: false,
-                    expandIcon: const Icon(YaruIcons.pan_end),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              model.description,
-                            ),
-                          ),
-                        ),
-                      ],
                     ),
                   ),
                 ],
               ),
             ),
+          ],
+        ),
+      ),
     );
   }
 }
