fceb3404343efa502fcbb3b518c972ef5f2d077c
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2022-12-13 16:35:24 +0100

    Clean up app.dart and settingspage (#638)
    * Clean up app.dart and settingspage
    
    * Update sources description

--- lib/app/common/constants.dart
+++ lib/app/common/constants.dart
@@ -41,3 +41,4 @@ const kFakeReviewText =
     'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
 
 const kBorderContainerBgDark = Color.fromARGB(255, 46, 46, 46);
+const badgeTextStyle = TextStyle(color: Colors.white, fontSize: 10);
--- lib/app/updates/updates_page.dart
+++ lib/app/updates/updates_page.dart
@@ -1,11 +1,45 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
-import 'package:software/l10n/l10n.dart';
+import 'package:software/app/common/constants.dart';
+import 'package:software/app/common/indeterminate_circular_progress_icon.dart';
 import 'package:software/app/updates/package_updates_page.dart';
 import 'package:software/app/updates/snap_updates_page.dart';
+import 'package:software/l10n/l10n.dart';
 import 'package:yaru_icons/yaru_icons.dart';
 
 class UpdatesPage extends StatefulWidget {
-  const UpdatesPage({super.key});
+  const UpdatesPage({
+    super.key,
+    this.onTabTapped,
+    this.tabIndex = 0,
+  });
+
+  final Function(int)? onTabTapped;
+  final int tabIndex;
+
+  static Widget create(
+    BuildContext context,
+    Function(int)? onTabTapped,
+    int tabIndex,
+  ) =>
+      UpdatesPage(
+        tabIndex: tabIndex,
+        onTabTapped: onTabTapped,
+      );
+
+  static Widget createTitle(BuildContext context) => Text(context.l10n.updates);
+
+  static Widget createIcon({
+    required BuildContext context,
+    required bool selected,
+    int? badgeCount,
+    bool? processing,
+  }) {
+    return _UpdatesIcon(
+      count: badgeCount ?? 0,
+      processing: processing ?? false,
+    );
+  }
 
   @override
   State<UpdatesPage> createState() => _UpdatesPageState();
@@ -15,10 +49,16 @@ class _UpdatesPageState extends State<UpdatesPage> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
+      initialIndex: widget.tabIndex,
       length: 2,
       child: Scaffold(
         appBar: AppBar(
           flexibleSpace: TabBar(
+            onTap: (value) {
+              if (widget.onTabTapped != null) {
+                widget.onTabTapped!(value);
+              }
+            },
             tabs: [
               Tab(
                 icon: _TabChild(
@@ -71,5 +111,47 @@ class _TabChild extends StatelessWidget {
         Text(label)
       ],
     );
+  }
+}
+
+class _UpdatesIcon extends StatelessWidget {
+  const _UpdatesIcon({
+    // ignore: unused_element
+    super.key,
+    required this.count,
+    required this.processing,
+  });
+
+  final int count;
+  final bool processing;
+
+  @override
+  Widget build(BuildContext context) {
+    if (processing && count > 0) {
+      return Badge(
+        position: BadgePosition.topEnd(),
+        badgeColor:
+            count > 0 ? Theme.of(context).primaryColor : Colors.transparent,
+        badgeContent: count > 0
+            ? Text(
+                count.toString(),
+                style: badgeTextStyle,
+              )
+            : null,
+        child: const IndeterminateCircularProgressIcon(),
+      );
+    } else if (processing && count == 0) {
+      return const IndeterminateCircularProgressIcon();
+    } else if (!processing && count > 0) {
+      return Badge(
+        badgeColor: Theme.of(context).primaryColor,
+        badgeContent: Text(
+          count.toString(),
+          style: badgeTextStyle,
+        ),
+        child: const Icon(YaruIcons.synchronizing),
+      );
+    }
+    return const Icon(YaruIcons.synchronizing);
   }
 }
