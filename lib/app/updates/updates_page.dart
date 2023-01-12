43cfd812715c07ba043e3603e7add0c3625b96fc
Feichtmeier <frederik.feichtmeier@gmail.com>
2023-01-12 13:49:25 +0100

    Flexible

--- lib/app/updates/updates_page.dart
+++ lib/app/updates/updates_page.dart
@@ -119,8 +119,7 @@ class _TabChild extends StatelessWidget {
         const SizedBox(
           width: 10,
         ),
-        ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 130),
+        Flexible(
           child: Text(
             label,
             overflow: TextOverflow.ellipsis,
