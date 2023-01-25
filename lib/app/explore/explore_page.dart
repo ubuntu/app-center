5c1884828661b8efe40af2af93274ed9ce0f379b
Dennis Loose <dennis.loose@canonical.com>
2023-01-25 12:28:58 +0100

    ExplorePage: clear search on sidebar event

--- lib/app/explore/explore_page.dart
+++ lib/app/explore/explore_page.dart
@@ -15,8 +15,11 @@
  *
  */
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:software/app/app_model.dart';
 import 'package:software/app/common/search_field.dart';
 import 'package:software/app/explore/explore_error_page.dart';
 import 'package:software/app/explore/explore_model.dart';
@@ -31,7 +34,7 @@ import 'package:ubuntu_service/ubuntu_service.dart';
 import 'package:yaru_icons/yaru_icons.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
 
-class ExplorePage extends StatelessWidget {
+class ExplorePage extends StatefulWidget {
   const ExplorePage({Key? key, required this.online}) : super(key: key);
 
   final bool online;
@@ -66,6 +69,28 @@ class ExplorePage extends StatelessWidget {
           : const Icon(YaruIcons.compass);
 
   @override
+  State<ExplorePage> createState() => _ExplorePageState();
+}
+
+class _ExplorePageState extends State<ExplorePage> {
+  StreamSubscription<bool>? _sidebarEventListener;
+  @override
+  void initState() {
+    super.initState();
+    final model = context.read<ExploreModel>();
+    _sidebarEventListener = context
+        .read<AppModel>()
+        .sidebarEvents
+        .listen((_) => model.setSearchQuery(''));
+  }
+
+  @override
+  void dispose() {
+    _sidebarEventListener?.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final showErrorPage = context.select((ExploreModel m) => m.showErrorPage);
     final showSearchPage = context.select((ExploreModel m) => m.showSearchPage);
@@ -75,11 +100,12 @@ class ExplorePage extends StatelessWidget {
     return Scaffold(
       appBar: YaruWindowTitleBar(
         title: SearchField(
+          key: ValueKey(showSearchPage),
           searchQuery: searchQuery,
           onChanged: setSearchQuery,
         ),
       ),
-      body: !online
+      body: !widget.online
           ? const OfflinePage()
           : showErrorPage
               ? const ExploreErrorPage()
