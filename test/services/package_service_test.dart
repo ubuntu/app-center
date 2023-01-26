9e6b9ab7afaf932c2aa76284d149fdbdc4638251
Dennis Loose <dennis.loose@canonical.com>
2023-01-26 12:12:32 +0100

    fix test

--- test/services/package_service_test.dart
+++ test/services/package_service_test.dart
@@ -563,8 +563,9 @@ void main() {
       );
       return emitFinishedEvent(controller);
     });
-    when(() => updateTransaction.getDetails([firefoxPackageId]))
-        .thenAnswer((_) => emitFinishedEvent(controller));
+    when(
+      () => updateTransaction.getDetails(any(that: contains(firefoxPackageId))),
+    ).thenAnswer((_) => emitFinishedEvent(controller));
     return updateTransaction;
   }
 
