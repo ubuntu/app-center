ff4a7e8bde978bd447b5638c1e47f943b1e836de
Dennis Loose <dennis.loose@canonical.com>
2023-01-31 13:58:32 +0100

    Snap progress indicator (#916)
    * add SnapdChange extension to calculate progress
    
    * SnapService: emit signal when progress changes
    
    * SnapControls: show progress of ongoing change

--- lib/app/common/snap/snap_controls.dart
+++ lib/app/common/snap/snap_controls.dart
@@ -21,6 +21,7 @@ import 'package:provider/provider.dart';
 import 'package:software/app/common/snap/snap_channel_button.dart';
 import 'package:software/app/common/snap/snap_model.dart';
 import 'package:software/l10n/l10n.dart';
+import 'package:software/snapd_change_x.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
 
 class SnapControls extends StatelessWidget {
@@ -46,17 +47,18 @@ class SnapControls extends StatelessWidget {
       runSpacing: 10,
       children: model.snapChangeInProgress
           ? [
-              const SizedBox(
+              SizedBox(
                 height: 20,
                 child: YaruCircularProgressIndicator(
                   strokeWidth: 3,
+                  value: model.change?.progress,
                 ),
               ),
               if (model.change != null)
                 Text(
                   getChangeMessage(
                     context: context,
-                    changeKind: model.change?.kind,
+                    changeKind: model.change!.kind,
                   ),
                 ),
             ]
