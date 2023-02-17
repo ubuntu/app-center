04e0d8d910294afb45a3d0420902a7011ac5d56e
MadsRH <madsrh@gmail.com>
2023-02-17 22:01:03 +0100

    Add files via upload

---
+++ lib/app/common/expandable_title.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class YaruExpandableTitle extends StatelessWidget {
+  final String title;
+
+  const YaruExpandableTitle({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 17),
+      overflow: TextOverflow.ellipsis,
+      maxLines: 1,
+    );
+  }
+}
