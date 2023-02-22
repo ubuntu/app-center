13c3da0274048d3ccca5fc77d404feb3025419b3
MadsRH <madsrh@gmail.com>
2023-02-23 00:00:18 +0100

    Add dialog

--- lib/app/collection/simple_snap_controls.dart
+++ lib/app/collection/simple_snap_controls.dart
@@ -116,9 +116,10 @@ class SimpleSnapControls extends StatelessWidget {
                           context: context,
                           builder: (BuildContext context) {
                             return AlertDialog(
-                              title: Text(context.l10n.confirm),
+                              title: Text(context.l10n
+                                  .removePackage(model.snap.apps.first.name)),
                               content: Text(
-                                '${context.l10n.remove} ${model.snap.apps.first.name}?',
+                                context.l10n.confirmRemove,
                               ),
                               actions: <Widget>[
                                 OutlinedButton(
@@ -128,6 +129,9 @@ class SimpleSnapControls extends StatelessWidget {
                                   },
                                 ),
                                 ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor:
+                                          Theme.of(context).colorScheme.error),
                                   child: Text(context.l10n.remove),
                                   onPressed: () {
                                     model.remove(context.l10n.done);
