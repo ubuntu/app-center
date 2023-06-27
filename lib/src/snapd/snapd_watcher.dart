11435022bb932923246b632d80996fab3fd73d7d
J-P Nurmi <jpnurmi@ubuntu.com>
2023-06-27 12:07:29 +0200

    feat: watcher (#16)

---
+++ lib/src/snapd/snapd_watcher.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:collection/collection.dart';
+import 'package:snapd/snapd.dart';
+
+mixin SnapdWatcher on SnapdClient {
+  Stream<SnapdChange> watchChange(
+    String id, {
+    Duration interval = const Duration(milliseconds: 100),
+  }) {
+    return Stream.periodic(interval, (_) => getChange(id))
+        .asyncMap((response) async => await response)
+        .distinct();
+  }
+
+  Stream<List<String>> watchChanges({
+    String? name,
+    Duration interval = const Duration(milliseconds: 100),
+  }) {
+    return Stream.periodic(interval, (_) => getChanges(name: name))
+        .asyncMap((response) async => await response)
+        .map((changes) => changes.map((c) => c.id).sorted())
+        .distinct(const ListEquality().equals);
+  }
+}
