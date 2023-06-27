654505256f598df17eeb7e991a717f52445dba5a
J-P Nurmi <jpnurmi@ubuntu.com>
2023-06-27 10:16:24 +0200

    feat: yaru & handy (#13)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:yaru/yaru.dart';
+import 'package:yaru_widgets/yaru_widgets.dart';
 
-void main() {
-  runApp(const MaterialApp());
+Future<void> main() async {
+  await YaruWindowTitleBar.ensureInitialized();
+
+  runApp(const StoreApp());
+}
+
+class StoreApp extends StatelessWidget {
+  const StoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return YaruTheme(
+      builder: (context, yaru, child) => MaterialApp(
+        theme: yaru.theme,
+        darkTheme: yaru.darkTheme,
+        debugShowCheckedModeBanner: false,
+        builder: (context, child) => Scaffold(
+          appBar: const YaruWindowTitleBar(),
+          body: child,
+        ),
+      ),
+    );
+  }
 }
