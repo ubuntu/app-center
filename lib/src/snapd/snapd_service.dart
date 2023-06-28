5e83e746f4e0209dff78ebfb93281ee89f9159eb
Dennis Loose <dennis.loose@canonical.com>
2023-06-28 14:36:28 +0200

    refactor(snapd_service): add waitChange helper

--- lib/src/snapd/snapd_service.dart
+++ lib/src/snapd/snapd_service.dart
@@ -2,4 +2,7 @@ import 'package:snapd/snapd.dart';
 
 import 'snapd_watcher.dart';
 
-class SnapdService extends SnapdClient with SnapdWatcher {}
+class SnapdService extends SnapdClient with SnapdWatcher {
+  Future<void> waitChange(String changeId) =>
+      watchChange(changeId).firstWhere((change) => change.ready);
+}
