7f113fea93b03f96e08285e7868738fc8476d93c
Dennis Loose <dennis.loose@canonical.com>
2023-07-04 14:25:47 +0200

    feat: add minimal search page

--- lib/src/search/search_page.dart
+++ lib/src/search/search_page.dart
@@ -1,21 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:snapd/snapd.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
 
-class SearchPage extends StatelessWidget {
-  const SearchPage({super.key, this.query});
+import '/widgets.dart';
+import 'search_provider.dart';
 
-  final String? query;
+class SearchPage extends ConsumerWidget {
+  const SearchPage({super.key, required this.query});
+
+  final String query;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final featured = ref.watch(searchProvider(query));
+    return featured.when(
+      data: (data) => _CategoryView(data, query),
+      error: (error, stack) => ErrorWidget(error),
+      loading: () => const Center(child: YaruCircularProgressIndicator()),
+    );
+  }
+}
+
+class _CategoryView extends ConsumerWidget {
+  const _CategoryView(this.snaps, this.query);
+
+  final List<Snap> snaps;
+  final String query;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final l10n = AppLocalizations.of(context);
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const YaruBackButton(),
         Padding(
           padding: const EdgeInsets.all(kYaruPagePadding),
-          child: Text('Search results for: $query'),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const YaruBackButton(),
+              Text(
+                l10n.searchPageTitle(query),
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+            ],
+          ),
         ),
+        Expanded(child: SnapGrid(snaps: snaps)),
       ],
     );
   }
