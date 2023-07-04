23ebba4358f436825b194af0984f14de0a117c9d
Dennis Loose <dennis.loose@canonical.com>
2023-07-04 17:23:19 +0200

    feat: add basic detail page UI

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,4 +1,5 @@
 export 'src/widgets/constants.dart';
 export 'src/widgets/snap_card.dart';
 export 'src/widgets/snap_grid.dart';
+export 'src/widgets/snap_icon.dart';
 export 'src/widgets/snap_title.dart';
