e63940b4a7184eb74c549f7a0ec1ce43261bdf47
Dennis Loose <dennis.loose@canonical.com>
2023-07-06 09:53:31 +0200

    refactor: use markdownBody finder from ubuntu_test

--- test/test_utils.dart
+++ test/test_utils.dart
@@ -1,6 +1,5 @@
 import 'package:app_store/l10n.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 extension WidgetTesterX on WidgetTester {
@@ -11,15 +10,5 @@ extension WidgetTesterX on WidgetTester {
       localizationsDelegates: localizationsDelegates,
       home: Scaffold(body: Builder(builder: builder)),
     ));
-  }
-}
-
-extension CommonFindersX on CommonFinders {
-  /// Finds [MarkdownBody] by [data].
-  Finder markdownBody(String data, {bool skipOffstage = true}) {
-    return byWidgetPredicate(
-      (w) => w is MarkdownBody && w.data == data,
-      skipOffstage: skipOffstage,
-    );
   }
 }
