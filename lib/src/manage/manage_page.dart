89e7f790e50738a3181ce1cb716bb07db60997f9
Dennis Loose <dennis.loose@canonical.com>
2023-07-06 12:14:43 +0200

    feat: minimal manage page UI improvements:
    - sort snaps
    - add icon
    - add padding
    - navigate to detail page on tap

--- lib/src/manage/manage_page.dart
+++ lib/src/manage/manage_page.dart
@@ -4,6 +4,9 @@ import 'package:snapd/snapd.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
 
 import '/l10n.dart';
+import '/routes.dart';
+import '/snapx.dart';
+import '/widgets.dart';
 import 'manage_provider.dart';
 
 class ManagePage extends ConsumerWidget {
@@ -31,14 +34,20 @@ class _ManageView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    snaps.sort(((a, b) =>
+        a.titleOrName.toLowerCase().compareTo(b.titleOrName.toLowerCase())));
     return ListView.builder(
+      padding: const EdgeInsets.all(kYaruPagePadding),
       itemCount: snaps.length,
       itemBuilder: (context, index) {
         final snap = snaps[index];
         return ListTile(
           key: ValueKey(snap.id),
-          title: Text(snap.name),
+          leading: SnapIcon(iconUrl: snap.iconUrl),
+          title: Text(snap.titleOrName),
           subtitle: Text(snap.summary),
+          onTap: () =>
+              Navigator.pushNamed(context, Routes.detail, arguments: snap.name),
         );
       },
     );
