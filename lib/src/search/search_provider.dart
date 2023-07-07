9371dbeed12e6a39b1fdf9bd49eab8db2cb60bd9
Dennis Loose <dennis.loose@canonical.com>
2023-07-07 12:27:44 +0200

    refactor(autocomplete): move cancellation logic to provider

--- lib/src/search/search_provider.dart
+++ lib/src/search/search_provider.dart
@@ -1,9 +1,24 @@
+import 'dart:async';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ubuntu_service/ubuntu_service.dart';
 
 import '/snapd.dart';
 
-final searchProvider = FutureProvider.autoDispose.family((ref, String query) {
+final searchProvider = FutureProvider.family((ref, String query) {
   final snapd = getService<SnapdService>();
   return snapd.find(query: query);
+});
+
+final queryProvider = StateProvider<String>((_) => '');
+
+final autoCompleteProvider = FutureProvider((ref) async {
+  final query = ref.watch(queryProvider);
+  final completer = Completer();
+  ref.onDispose(completer.complete);
+  await Future.delayed(const Duration(milliseconds: 100));
+  if (query.isNotEmpty && !completer.isCompleted) {
+    return ref.watch(searchProvider(query).future);
+  }
+  return [];
 });
