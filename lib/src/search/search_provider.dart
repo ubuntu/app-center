949f7d05ae4bf9b2f5295d7977176c1cfc4d24ac
Dennis Loose <dennis.loose@canonical.com>
2023-07-10 12:12:22 +0200

    feat(search): add result sorting

--- lib/src/search/search_provider.dart
+++ lib/src/search/search_provider.dart
@@ -3,7 +3,9 @@ import 'dart:async';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ubuntu_service/ubuntu_service.dart';
 
+import '/l10n.dart';
 import '/snapd.dart';
+import '/snapx.dart';
 
 final searchProvider = FutureProvider.family((ref, String query) {
   final snapd = getService<SnapdService>();
@@ -21,4 +23,43 @@ final autoCompleteProvider = FutureProvider((ref) async {
     return ref.watch(searchProvider(query).future);
   }
   return [];
+});
+
+enum SnapSortOrder {
+  alphabetical,
+  downloadSize,
+  relevance;
+
+  String localize(AppLocalizations l10n) {
+    return switch (this) {
+      alphabetical => l10n.searchPageAlphabeticalLabel,
+      downloadSize => l10n.searchPageDownloadSizeLabel,
+      relevance => l10n.searchPageRelevanceLabel,
+    };
+  }
+}
+
+final sortOrderProvider = StateProvider.autoDispose((ref) {
+  return SnapSortOrder.relevance;
+});
+
+final sortedSearchProvider =
+    FutureProvider.family.autoDispose((ref, String query) {
+  return ref.watch(searchProvider(query).future).then((snaps) {
+    final sortOrder = ref.watch(sortOrderProvider);
+    if (sortOrder == SnapSortOrder.relevance) {
+      return snaps;
+    }
+
+    final sortedSnaps = snaps.toList();
+    sortedSnaps.sort(((a, b) => switch (sortOrder) {
+          SnapSortOrder.alphabetical => a.titleOrName.compareTo(b.titleOrName),
+          SnapSortOrder.downloadSize =>
+            a.downloadSize != null && b.downloadSize != null
+                ? a.downloadSize!.compareTo(b.downloadSize!)
+                : 0,
+          _ => 0,
+        }));
+    return sortedSnaps;
+  });
 });
