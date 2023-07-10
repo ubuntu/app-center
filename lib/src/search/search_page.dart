80a4def645e66b5001e56ff6c38412871bd7b19a
Dennis Loose <dennis.loose@canonical.com>
2023-07-10 12:12:22 +0200

    feat(search): add result sorting

--- lib/src/search/search_page.dart
+++ lib/src/search/search_page.dart
@@ -1,36 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:snapd/snapd.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
 
 import '/l10n.dart';
 import '/widgets.dart';
 import 'search_provider.dart';
 
-class SearchPage extends ConsumerWidget {
+class SearchPage extends StatelessWidget {
   const SearchPage({super.key, required this.query});
 
   final String query;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final featured = ref.watch(searchProvider(query));
-    return featured.when(
-      data: (data) => _CategoryView(data, query),
-      error: (error, stack) => ErrorWidget(error),
-      loading: () => const Center(child: YaruCircularProgressIndicator()),
-    );
-  }
-}
-
-class _CategoryView extends ConsumerWidget {
-  const _CategoryView(this.snaps, this.query);
-
-  final List<Snap> snaps;
-  final String query;
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context);
 
     return Column(
@@ -46,10 +28,38 @@ class _CategoryView extends ConsumerWidget {
                 l10n.searchPageTitle(query),
                 style: Theme.of(context).textTheme.headlineSmall,
               ),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  Text(l10n.searchPageSortByLabel),
+                  const SizedBox(width: 8),
+                  Consumer(builder: (context, ref, child) {
+                    final sortOrder = ref.watch(sortOrderProvider);
+                    return YaruPopupMenuButton<SnapSortOrder>(
+                      itemBuilder: (_) => SnapSortOrder.values
+                          .map((e) => PopupMenuItem(
+                                value: e,
+                                child: Text(e.localize(l10n)),
+                              ))
+                          .toList(),
+                      onSelected: (value) =>
+                          ref.read(sortOrderProvider.notifier).state = value,
+                      child: Text(sortOrder.localize(l10n)),
+                    );
+                  }),
+                ],
+              )
             ],
           ),
         ),
-        Expanded(child: SnapGrid(snaps: snaps)),
+        Expanded(child: Consumer(builder: (context, ref, child) {
+          final results = ref.watch(sortedSearchProvider(query));
+          return results.when(
+            data: (data) => SnapGrid(snaps: data),
+            error: (error, stack) => ErrorWidget(error),
+            loading: () => const Center(child: YaruCircularProgressIndicator()),
+          );
+        })),
       ],
     );
   }
