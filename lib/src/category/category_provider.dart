17b7231d63e5c97191bbb191f6294274a5b69000
J-P Nurmi <jpnurmi@ubuntu.com>
2023-07-12 13:55:06 +0200

    feat: cache store snaps and categories (#70)

--- lib/src/category/category_provider.dart
+++ lib/src/category/category_provider.dart
@@ -4,7 +4,7 @@ import 'package:ubuntu_service/ubuntu_service.dart';
 import '/snapd.dart';
 
 final categoryProvider =
-    FutureProvider.autoDispose.family((ref, String category) {
+    StreamProvider.autoDispose.family((ref, String category) {
   final snapd = getService<SnapdService>();
-  return snapd.find(category: category);
+  return snapd.getCategory(category);
 });
--- lib/src/snapd/snapd_service.dart
+++ lib/src/snapd/snapd_service.dart
@@ -1,8 +1,9 @@
 import 'package:snapd/snapd.dart';
 
+import 'snapd_cache.dart';
 import 'snapd_watcher.dart';
 
-class SnapdService extends SnapdClient with SnapdWatcher {
+class SnapdService extends SnapdClient with SnapdCache, SnapdWatcher {
   Future<void> waitChange(String changeId) =>
       watchChange(changeId).firstWhere((change) => change.ready);
 }
