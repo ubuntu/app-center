a5f45e26b3b21d138a8e7eda745c5518cf3a075b
Dennis Loose <dennis.loose@canonical.com>
2023-07-25 09:35:06 +0000

    feat: add localized category names to title (#1296)

--- lib/src/widgets/snap_title.dart
+++ lib/src/widgets/snap_title.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:snapd/snapd.dart';
@@ -22,9 +23,9 @@ class SnapTitle extends StatelessWidget {
     final l10n = AppLocalizations.of(context);
     final textTheme = Theme.of(context).textTheme;
     final titleTextStyle =
-        large ? textTheme.headlineLarge! : textTheme.titleMedium!;
+        large ? textTheme.headlineSmall! : textTheme.titleMedium!;
     final publisherTextStyle =
-        large ? textTheme.headlineSmall! : textTheme.bodyMedium!;
+        large ? textTheme.bodyMedium! : textTheme.bodyMedium!;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisSize: MainAxisSize.min,
@@ -59,7 +60,53 @@ class SnapTitle extends StatelessWidget {
               ),
           ],
         ),
+        if (large) ...[
+          const SizedBox(height: 8),
+          Row(
+            children: snap.categories
+                .whereNot((c) => c.name == 'featured')
+                .map((c) => Text(c.localize(l10n)))
+                .separatedBy(const Text(', ')),
+          )
+        ]
       ],
     );
+  }
+}
+
+extension on Iterable<Widget> {
+  List<Widget> separatedBy(Widget separator) => [
+        for (var i = 0; i < length; i++) ...[
+          elementAt(i),
+          if (i < length - 1) separator,
+        ]
+      ];
+}
+
+extension SnapCategoryL10n on SnapCategory {
+  String localize(AppLocalizations l10n) {
+    return switch (name) {
+      'art-and-design' => l10n.snapCategoryArtAndDesign,
+      'books-and-reference' => l10n.snapCategoryBooksAndReference,
+      'development' => l10n.snapCategoryDevelopment,
+      'devices-and-iot' => l10n.snapCategoryDevicesAndIot,
+      'education' => l10n.snapCategoryEducation,
+      'entertainment' => l10n.snapCategoryEntertainment,
+      'featured' => l10n.snapCategoryFeatured,
+      'finance' => l10n.snapCategoryFinance,
+      'games' => l10n.snapCategoryGames,
+      'health-and-fitness' => l10n.snapCategoryHealthAndFitness,
+      'music-and-audio' => l10n.snapCategoryMusicAndAudio,
+      'news-and-weather' => l10n.snapCategoryNewsAndWeather,
+      'personalisation' => l10n.snapCategoryPersonalisation,
+      'photo-and-video' => l10n.snapCategoryPhotoAndVideo,
+      'productivity' => l10n.snapCategoryProductivity,
+      'science' => l10n.snapCategoryScience,
+      'security' => l10n.snapCategorySecurity,
+      'server-and-cloud' => l10n.snapCategoryServerAndCloud,
+      'social' => l10n.snapCategorySocial,
+      'utilities' => l10n.snapCategoryUtilities,
+      _ => name
+    };
   }
 }
