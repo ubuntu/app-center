a940127f7e486f3db8297d929e5b383fdcc998b2
Dennis Loose <dennis.loose@canonical.com>
2023-08-03 15:34:15 +0000

    test: ManageModel (#1312)

---
+++ test/manage_model_test.dart
@@ -0,0 +1,33 @@
+import 'package:app_store/src/manage/manage_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:snapd/snapd.dart';
+
+import 'test_utils.dart';
+
+void main() {
+  test('no updates available', () async {
+    final snapd = createMockSnapdService(
+      installedSnaps: const [Snap(name: 'firefox'), Snap(name: 'thunderbird')],
+    );
+    final updatesModel = createMockUpdatesModel();
+    final model = ManageModel(snapd: snapd, updatesModel: updatesModel);
+    await model.init();
+
+    expect(model.nonRefreshableSnaps,
+        equals(const [Snap(name: 'firefox'), Snap(name: 'thunderbird')]));
+    expect(model.refreshableSnaps, isEmpty);
+  });
+  test('update available', () async {
+    final snapd = createMockSnapdService(
+      installedSnaps: const [Snap(name: 'firefox'), Snap(name: 'thunderbird')],
+    );
+    final updatesModel =
+        createMockUpdatesModel(refreshableSnapNames: ['firefox']);
+    final model = ManageModel(snapd: snapd, updatesModel: updatesModel);
+    await model.init();
+
+    expect(
+        model.nonRefreshableSnaps, equals(const [Snap(name: 'thunderbird')]));
+    expect(model.refreshableSnaps, equals(const [Snap(name: 'firefox')]));
+  });
+}
