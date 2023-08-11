8e66362fd64bb2c1b3bf4a06087382286ee54e11
Dennis Loose <dennis.loose@canonical.com>
2023-08-11 10:54:06 +0000

    feat: add 'snap://' argument handler (#1327)
    * feat: add 'snap://' argument handler
    
    * improve code legibility

---
+++ test/initial_route_test.dart
@@ -0,0 +1,74 @@
+import 'package:app_store/src/store/store_providers.dart';
+import 'package:app_store/src/store/store_routes.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockInitialRouteListener extends Mock {
+  void call(String? previous, String? next);
+}
+
+void main() {
+  test('search query', () {
+    final container = ProviderContainer(overrides: [
+      commandLineProvider.overrideWith((ref) => ['--search', 'foo'])
+    ]);
+    addTearDown(container.dispose);
+
+    final listener = MockInitialRouteListener();
+    container.listen<String?>(
+      initialRouteProvider,
+      listener,
+      fireImmediately: true,
+    );
+
+    verify(listener(null, StoreRoutes.namedSearch(query: 'foo'))).called(1);
+  });
+
+  test('snap name', () {
+    final container = ProviderContainer(overrides: [
+      commandLineProvider.overrideWith((ref) => ['--snap', 'bar'])
+    ]);
+    addTearDown(container.dispose);
+
+    final listener = MockInitialRouteListener();
+    container.listen<String?>(
+      initialRouteProvider,
+      listener,
+      fireImmediately: true,
+    );
+
+    verify(listener(null, StoreRoutes.namedDetail(name: 'bar'))).called(1);
+  });
+
+  test('snap url', () {
+    final container = ProviderContainer(overrides: [
+      commandLineProvider.overrideWith((ref) => ['snap://bar'])
+    ]);
+    addTearDown(container.dispose);
+
+    final listener = MockInitialRouteListener();
+    container.listen<String?>(
+      initialRouteProvider,
+      listener,
+      fireImmediately: true,
+    );
+
+    verify(listener(null, StoreRoutes.namedDetail(name: 'bar'))).called(1);
+  });
+
+  test('no arguments', () {
+    final container = ProviderContainer(
+        overrides: [commandLineProvider.overrideWith((ref) => [])]);
+    addTearDown(container.dispose);
+
+    final listener = MockInitialRouteListener();
+    container.listen<String?>(
+      initialRouteProvider,
+      listener,
+      fireImmediately: true,
+    );
+
+    verify(listener(null, null)).called(1);
+  });
+}
