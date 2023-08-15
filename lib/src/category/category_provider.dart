671e090d11de0c8d47e1c8a6f48afe213ea4f838
Dennis Loose <dennis.loose@canonical.com>
2023-08-15 16:54:36 +0200

    todo: deprecate old categoryProvider

--- lib/src/category/category_provider.dart
+++ lib/src/category/category_provider.dart
@@ -3,6 +3,7 @@ import 'package:ubuntu_service/ubuntu_service.dart';
 
 import '/snapd.dart';
 
+// TODO: remove - has been replaced by generalized search provider
 final categoryProvider =
     StreamProvider.autoDispose.family((ref, SnapCategoryEnum category) {
   final snapd = getService<SnapdService>();
