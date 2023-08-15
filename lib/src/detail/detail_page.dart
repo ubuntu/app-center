1caf79d66019dde766f8eb06cf36df44d25f2e5a
Dennis Loose <dennis.loose@canonical.com>
2023-08-15 11:07:04 +0200

    move snap l10n extensions into separate file

--- lib/src/detail/detail_page.dart
+++ lib/src/detail/detail_page.dart
@@ -509,20 +509,3 @@ class _ChannelDropdownEntry extends StatelessWidget {
     );
   }
 }
-
-extension SnapdChangeL10n on SnapdChange {
-  String? localize(AppLocalizations l10n) => switch (kind) {
-        'install-snap' => l10n.snapActionInstallingLabel,
-        'remove-snap' => l10n.snapActionRemovingLabel,
-        _ => null,
-      };
-}
-
-extension SnapConfinementL10n on SnapConfinement {
-  String localize(AppLocalizations l10n) => switch (this) {
-        SnapConfinement.classic => l10n.snapConfinementClassic,
-        SnapConfinement.devmode => l10n.snapConfinementDevmode,
-        SnapConfinement.strict => l10n.snapConfinementStrict,
-        _ => name,
-      };
-}
---
+++ lib/src/snapd/snap_l10n.dart
@@ -0,0 +1,20 @@
+import 'package:snapd/snapd.dart';
+
+import '/l10n.dart';
+
+extension SnapdChangeL10n on SnapdChange {
+  String? localize(AppLocalizations l10n) => switch (kind) {
+        'install-snap' => l10n.snapActionInstallingLabel,
+        'remove-snap' => l10n.snapActionRemovingLabel,
+        _ => null,
+      };
+}
+
+extension SnapConfinementL10n on SnapConfinement {
+  String localize(AppLocalizations l10n) => switch (this) {
+        SnapConfinement.classic => l10n.snapConfinementClassic,
+        SnapConfinement.devmode => l10n.snapConfinementDevmode,
+        SnapConfinement.strict => l10n.snapConfinementStrict,
+        _ => name,
+      };
+}
