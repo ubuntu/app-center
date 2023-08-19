6c1a1e6b4ff6f9a6794599b32e4d33951ea880df
Dennis Loose <dennis.loose@canonical.com>
2023-08-19 09:51:26 +0000

    feat: update explore page (#1345)
    * add missing l10n string
    
    * fix featured productivity snaps
    
    * remove obsolete explore provider
    
    * update explore page
    
    * add comments

--- lib/src/explore/explore_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:ubuntu_service/ubuntu_service.dart';
-
-import '/snapd.dart';
-
-final featuredProvider = StreamProvider.autoDispose((ref) {
-  final snapd = getService<SnapdService>();
-  return snapd.getCategory('featured');
-});
