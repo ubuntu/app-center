74058e32d376a9d3281449373c7b349629ecdcc2
Dennis Loose <dennis.loose@canonical.com>
2023-09-18 14:18:08 +0200

    fix(log): create logfile in XDG_DATA_HOME by default

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github/github.dart';
 import 'package:gtk/gtk.dart';
 import 'package:packagekit/packagekit.dart';
+import 'package:path/path.dart' as p;
 import 'package:snapcraft_launcher/snapcraft_launcher.dart';
 import 'package:ubuntu_logger/ubuntu_logger.dart';
 import 'package:ubuntu_service/ubuntu_service.dart';
+import 'package:xdg_directories/xdg_directories.dart' as xdg;
 import 'package:yaru_widgets/yaru_widgets.dart';
 
 import 'appstream.dart';
@@ -47,7 +50,14 @@ Future<void> main(List<String> args) async {
 
   await initDefaultLocale();
 
-  Logger.setup();
+  final binaryName = p.basename(Platform.resolvedExecutable);
+  Logger.setup(
+    path: p.join(
+      xdg.dataHome.path,
+      binaryName,
+      '$binaryName.log',
+    ),
+  );
 
   runApp(const ProviderScope(child: StoreApp()));
 }
