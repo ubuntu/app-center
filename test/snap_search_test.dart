1d3e3fab3c2af8e56ea5ee8db6c84275a9f38e44
Dennis Loose <dennis.loose@canonical.com>
2023-09-26 19:06:47 +0200

    feat: filter snap search query (#1412)
    * feat: filter snap search query
    
    * doc: add reference

---
+++ test/snap_search_test.dart
@@ -0,0 +1,12 @@
+import 'package:app_center/snapd.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('cleaned query', () {
+    const searchParamters = SnapSearchParameters(
+      query: 'a+b=c&d|e>f<g!h(i)j{k}l[m]n^o"p~q*r?s:t\\u/v',
+    );
+    expect(searchParamters.cleanedQuery,
+        equals('a b c d e f g h i j k l m n o p q r s t u v'));
+  });
+}
