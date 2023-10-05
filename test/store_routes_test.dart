19a53116f3d5e92d562036d820025eb744955e72
Dennis Loose <dennis.loose@canonical.com>
2023-10-05 14:40:35 +0200

    test(store_routes): add deb routes

--- test/store_routes_test.dart
+++ test/store_routes_test.dart
@@ -10,6 +10,8 @@ void main() {
     expect(StoreRoutes.snapOf(root), isNull);
     expect(StoreRoutes.isSearch(root), isFalse);
     expect(StoreRoutes.queryOf(root), isNull);
+    expect(StoreRoutes.isDeb(root), isFalse);
+    expect(StoreRoutes.debOf(root), isNull);
 
     const snap = RouteSettings(name: '/snap?snap=foo');
     expect(StoreRoutes.routeOf(snap), equals('/snap'));
@@ -17,6 +19,8 @@ void main() {
     expect(StoreRoutes.snapOf(snap), equals('foo'));
     expect(StoreRoutes.isSearch(snap), isFalse);
     expect(StoreRoutes.queryOf(snap), isNull);
+    expect(StoreRoutes.isDeb(snap), isFalse);
+    expect(StoreRoutes.debOf(snap), isNull);
 
     const search = RouteSettings(name: '/search?query=bar');
     expect(StoreRoutes.routeOf(search), equals('/search'));
@@ -24,6 +28,8 @@ void main() {
     expect(StoreRoutes.snapOf(search), isNull);
     expect(StoreRoutes.isSearch(search), isTrue);
     expect(StoreRoutes.queryOf(search), equals('bar'));
+    expect(StoreRoutes.isDeb(search), isFalse);
+    expect(StoreRoutes.debOf(search), isNull);
 
     const searchSnap = RouteSettings(name: '/snap?query=bar&snap=foo');
     expect(StoreRoutes.routeOf(searchSnap), equals('/snap'));
@@ -31,6 +37,17 @@ void main() {
     expect(StoreRoutes.snapOf(searchSnap), equals('foo'));
     expect(StoreRoutes.isSearch(searchSnap), isFalse);
     expect(StoreRoutes.queryOf(searchSnap), equals('bar'));
+    expect(StoreRoutes.isDeb(searchSnap), isFalse);
+    expect(StoreRoutes.debOf(searchSnap), isNull);
+
+    const deb = RouteSettings(name: '/deb?deb=baz');
+    expect(StoreRoutes.routeOf(deb), equals('/deb'));
+    expect(StoreRoutes.isSnap(deb), isFalse);
+    expect(StoreRoutes.snapOf(deb), isNull);
+    expect(StoreRoutes.isSearch(deb), isFalse);
+    expect(StoreRoutes.queryOf(deb), isNull);
+    expect(StoreRoutes.isDeb(deb), isTrue);
+    expect(StoreRoutes.debOf(deb), equals('baz'));
   });
 
   test('named route', () {
@@ -43,5 +60,6 @@ void main() {
     expect(StoreRoutes.namedSearch(query: 'bar'), equals('/search?query=bar'));
     expect(StoreRoutes.namedSearchSnap(query: 'bar', name: 'foo'),
         equals('/snap?snap=foo&query=bar'));
+    expect(StoreRoutes.namedDeb(id: 'baz'), equals('/deb?deb=baz'));
   });
 }
