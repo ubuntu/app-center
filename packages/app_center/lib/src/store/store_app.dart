4cd4ba7fc80c3af8183792b0dfe0e7aab1e0fc9c
Enzo Conty <contact@enzoconty.dev>
2023-12-27 11:21:25 +0100

    fix: Fixing the window name in mission control

--- packages/app_center/lib/src/store/store_app.dart
+++ packages/app_center/lib/src/store/store_app.dart
@@ -43,55 +43,72 @@ class _StoreAppState extends ConsumerState<StoreApp> {
         localizationsDelegates: localizationsDelegates,
         navigatorKey: ref.watch(materialAppNavigatorKeyProvider),
         supportedLocales: supportedLocales,
-        home: Scaffold(
-          appBar: YaruWindowTitleBar(
-            title: ConstrainedBox(
-              constraints: const BoxConstraints(maxWidth: kSearchBarWidth),
-              child: SearchField(
-                onSearch: (query) =>
-                    _navigator.pushAndRemoveSearch(query: query),
-                onSnapSelected: (name) => _navigator.pushSnap(name: name),
-                onDebSelected: (id) => _navigator.pushDeb(id: id),
+        home: YaruWindowTitleSetter(
+          child: Scaffold(
+            appBar: YaruWindowTitleBar(
+              title: ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: kSearchBarWidth),
+                child: SearchField(
+                  onSearch: (query) =>
+                      _navigator.pushAndRemoveSearch(query: query),
+                  onSnapSelected: (name) => _navigator.pushSnap(name: name),
+                  onDebSelected: (id) => _navigator.pushDeb(id: id),
+                ),
               ),
             ),
-          ),
-          body: YaruMasterDetailPage(
-            navigatorKey: _navigatorKey,
-            navigatorObservers: [StoreObserver(ref)],
-            initialRoute: ref.watch(initialRouteProvider),
-            length: pages.length,
-            tileBuilder: (context, index, selected, availableWidth) =>
-                pages[index].tileBuilder(context, selected),
-            pageBuilder: (context, index) => pages[index].pageBuilder(context),
-            layoutDelegate: const YaruMasterFixedPaneDelegate(
-              paneWidth: kPaneWidth,
+            body: YaruMasterDetailPage(
+              navigatorKey: _navigatorKey,
+              navigatorObservers: [StoreObserver(ref)],
+              initialRoute: ref.watch(initialRouteProvider),
+              length: pages.length,
+              tileBuilder: (context, index, selected, availableWidth) =>
+                  pages[index].tileBuilder(context, selected),
+              pageBuilder: (context, index) =>
+                  pages[index].pageBuilder(context),
+              layoutDelegate: const YaruMasterFixedPaneDelegate(
+                paneWidth: kPaneWidth,
+              ),
+              breakpoint: 0, // always landscape
+              onGenerateRoute: (settings) =>
+                  switch (StoreRoutes.routeOf(settings)) {
+                StoreRoutes.deb => MaterialPageRoute(
+                    settings: settings,
+                    builder: (_) => DebPage(
+                          id: StoreRoutes.debOf(settings)!,
+                        )),
+                StoreRoutes.snap => MaterialPageRoute(
+                    settings: settings,
+                    builder: (_) => SnapPage(
+                      snapName: StoreRoutes.snapOf(settings)!,
+                    ),
+                  ),
+                StoreRoutes.search => MaterialPageRoute(
+                    settings: settings,
+                    builder: (_) => SearchPage(
+                      query: StoreRoutes.queryOf(settings),
+                      category: StoreRoutes.categoryOf(settings),
+                    ),
+                  ),
+                _ => null,
+              },
             ),
-            breakpoint: 0, // always landscape
-            onGenerateRoute: (settings) =>
-                switch (StoreRoutes.routeOf(settings)) {
-              StoreRoutes.deb => MaterialPageRoute(
-                  settings: settings,
-                  builder: (_) => DebPage(
-                        id: StoreRoutes.debOf(settings)!,
-                      )),
-              StoreRoutes.snap => MaterialPageRoute(
-                  settings: settings,
-                  builder: (_) => SnapPage(
-                    snapName: StoreRoutes.snapOf(settings)!,
-                  ),
-                ),
-              StoreRoutes.search => MaterialPageRoute(
-                  settings: settings,
-                  builder: (_) => SearchPage(
-                    query: StoreRoutes.queryOf(settings),
-                    category: StoreRoutes.categoryOf(settings),
-                  ),
-                ),
-              _ => null,
-            },
           ),
         ),
       ),
     );
+  }
+}
+
+class YaruWindowTitleSetter extends StatelessWidget {
+  const YaruWindowTitleSetter({required this.child, super.key});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context);
+    YaruWindow.of(context).setTitle(l10n.appCenterLabel);
+
+    return child;
   }
 }
