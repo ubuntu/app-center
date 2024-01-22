6bd3d0abb01c30144121c169b1f3d2390d2c46f6
Jesús Soto <jesus.soto@canonical.com>
2024-01-22 14:12:02 -0600

    refactor: choose text style dynamically depending on icon size

--- packages/app_center/lib/src/explore/explore_page.dart
+++ packages/app_center/lib/src/explore/explore_page.dart
@@ -180,6 +180,8 @@ class _Banner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final titleTextStyle = kIconSize > 40 ? textTheme.headlineSmall! : textTheme.titleMedium!;
     return Container(
       padding: EdgeInsets.all(padding),
       decoration: BoxDecoration(
@@ -200,10 +202,7 @@ class _Banner extends StatelessWidget {
                 children: [
                   Text(
                     slogan,
-                    style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                        color: _kForegroundColor,
-                        fontSize: fontSize,
-                        letterSpacing: 0.15),
+                    style: titleTextStyle.copyWith(color: _kForegroundColor),
                   ),
                   if (buttonLabel != null) ...[
                     const SizedBox(height: 24),
