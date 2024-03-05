ec2d1645e0df0ccc29168832e0ef83680610e72e
ashuntu <ashton.nelson@canonical.com>
2024-03-05 13:13:16 -0600

    feat: add snap carousel

--- packages/app_center/lib/src/games/games_page_featured.dart
+++ packages/app_center/lib/src/games/games_page_featured.dart
@@ -1,11 +1,111 @@
+import 'package:app_center/snapd.dart';
+import 'package:app_center/src/store/store_navigator.dart';
+import 'package:app_center/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:snapd/snapd.dart';
+import 'package:yaru_icons/yaru_icons.dart';
+import 'package:yaru_widgets/yaru_widgets.dart';
 
-class GamesPageFeatured extends ConsumerWidget {
+class GamesPageFeatured extends StatelessWidget {
   const GamesPageFeatured({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return const Text("I'm a Featured section!");
+  Widget build(BuildContext context) {
+    return const _FeaturedCarousel(
+      snapAmount: 10,
+      scrollDelay: Duration(seconds: 5),
+    );
+  }
+}
+
+class _FeaturedCarousel extends ConsumerStatefulWidget {
+  const _FeaturedCarousel(
+      {required this.snapAmount, required this.scrollDelay});
+
+  final Duration scrollDelay;
+  final int snapAmount;
+
+  @override
+  ConsumerState<_FeaturedCarousel> createState() => _FeaturedCarouselState();
+}
+
+class _FeaturedCarouselState extends ConsumerState<_FeaturedCarousel> {
+  late YaruCarouselController controller;
+  late Iterable<Snap> snaps;
+
+  @override
+  Widget build(BuildContext context) {
+    snaps = ref
+            .watch(snapSearchProvider(
+                const SnapSearchParameters(category: SnapCategoryEnum.games)))
+            .whenOrNull(data: (data) => data)
+            ?.take(5) ??
+        [];
+
+    controller = YaruCarouselController(
+        autoScroll: true, autoScrollDuration: const Duration(seconds: 5));
+
+    return MouseRegion(
+      onEnter: (_) => controller.cancelTimer(),
+      onExit: (_) => controller.startTimer(),
+      child: YaruCarousel(
+          controller: controller,
+          height: 260,
+          width: double.infinity,
+          nextIcon: const Icon(YaruIcons.go_next),
+          previousIcon: const Icon(YaruIcons.go_previous),
+          navigationControls: true,
+          children: snaps
+              .map((e) => _CarouselCard(
+                  snap: e,
+                  onTap: (snap) =>
+                      StoreNavigator.pushSnap(context, name: e.name)))
+              .toList()),
+    );
+  }
+}
+
+class _CarouselCard extends StatelessWidget {
+  const _CarouselCard({required this.snap, required this.onTap});
+
+  final Snap snap;
+  final ValueChanged<Snap> onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return YaruBanner(
+        padding: EdgeInsets.zero,
+        onTap: () => onTap(snap),
+        child:
+            Stack(alignment: Alignment.center, fit: StackFit.expand, children: [
+          SafeNetworkImage(
+            url: snap.screenshotUrls.first,
+            fit: BoxFit.cover,
+          ),
+          Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                      colors: [
+                    Colors.black.withOpacity(1.0),
+                    Colors.black.withOpacity(0.2)
+                  ])),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(snap.name,
+                        style: Theme.of(context).textTheme.displayMedium),
+                    Text(
+                      snap.summary,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    )
+                  ]))
+        ]));
   }
 }
