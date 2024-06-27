fa741f0a69068636d653eb99a3ca67e17015368a
ashuntu <ashton.nelson@canonical.com>
2024-06-27 09:53:13 -0500

    Add rated snap category list

--- packages/app_center/lib/snapd/snapd_service.dart
+++ packages/app_center/lib/snapd/snapd_service.dart
@@ -5,4 +5,19 @@ import 'package:snapd/snapd.dart';
 class SnapdService extends SnapdClient with SnapdCache, SnapdWatcher {
   Future<void> waitChange(String changeId) =>
       watchChange(changeId).firstWhere((change) => change.ready);
+
+  Future<Snap?> findById(String snapId) async {
+    final queryParams = {
+      'series': '16',
+      'remote': 'true',
+      'snap-id': snapId,
+    };
+    final result =
+        await getAssertions(assertion: 'snap-declaration', params: queryParams);
+    final declaration = SnapDeclaration.fromJson(result);
+    final findResult = await find(name: declaration.snapName);
+    return findResult
+        .where((element) => element.id == declaration.snapId)
+        .firstOrNull;
+  }
 }
