6a7aab117c92057de609232b5f5ff325f6e77655
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2024-08-22 18:17:52 +0200

    chore: Move l10n base to root

--- packages/app_center/lib/l10n.dart
+++ packages/app_center/lib/l10n.dart
@@ -1,3 +1,16 @@
-export 'package:app_center/src/l10n/app_localizations.dart';
+import 'package:app_center/src/l10n/app_localizations.dart';
+import 'package:flutter/widgets.dart';
+import 'package:ubuntu_localizations/ubuntu_localizations.dart';
 
-export '../src/l10n/l10n.dart';
+export 'package:app_center/src/l10n/app_localizations.dart';
+export 'package:ubuntu_localizations/ubuntu_localizations.dart';
+
+final List<Locale> supportedLocales = {
+  const Locale('en'),
+  ...List.of(AppLocalizations.supportedLocales)..remove(const Locale('en')),
+}.toList();
+
+const localizationsDelegates = <LocalizationsDelegate<dynamic>>[
+  ...AppLocalizations.localizationsDelegates,
+  ...GlobalUbuntuLocalizations.delegates,
+];
