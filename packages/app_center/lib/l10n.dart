2af408a97604830c77e4186ba8e1b565e9d6a015
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2024-08-23 17:18:10 +0200

    chore: Use correct app_localizations file

--- packages/app_center/lib/l10n.dart
+++ packages/app_center/lib/l10n.dart
@@ -1,8 +1,8 @@
-import 'package:app_center/src/l10n/app_localizations.dart';
+import 'package:app_center/src/l10n/app_localizations.g.dart';
 import 'package:flutter/widgets.dart';
 import 'package:ubuntu_localizations/ubuntu_localizations.dart';
 
-export 'package:app_center/src/l10n/app_localizations.dart';
+export 'package:app_center/src/l10n/app_localizations.g.dart';
 export 'package:ubuntu_localizations/ubuntu_localizations.dart';
 
 final List<Locale> supportedLocales = {
