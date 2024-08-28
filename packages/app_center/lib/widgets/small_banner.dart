3ee09a38f8652aeb7f953830b107ec62f5cdce01
ashuntu <ashton.nelson@canonical.com>
2024-08-28 17:04:14 -0500

    Remove outline on hover for rated cards

--- packages/app_center/lib/widgets/small_banner.dart
+++ packages/app_center/lib/widgets/small_banner.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yaru/yaru.dart';
 
+/// Slimmer version (no border or description) of the [YaruBanner].
 class SmallBanner extends StatelessWidget {
   const SmallBanner({required this.child, super.key, this.onTap});
 
@@ -11,18 +12,16 @@ class SmallBanner extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final borderRadius = BorderRadius.circular(kYaruBannerRadius);
-
     final light = theme.brightness == Brightness.light;
-
     final defaultSurfaceTintColor =
         light ? theme.cardColor : const Color.fromARGB(255, 126, 126, 126);
+
     return Material(
       color: Colors.transparent,
       borderRadius: borderRadius,
       child: InkWell(
         onTap: onTap,
-        borderRadius: borderRadius,
-        hoverColor: theme.colorScheme.onSurface.withOpacity(0.1),
+        hoverColor: Colors.transparent,
         child: Card(
           color: theme.cardColor,
           shadowColor: Colors.transparent,
