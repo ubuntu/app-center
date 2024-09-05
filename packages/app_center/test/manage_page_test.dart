b6fc5ad07c35c0d8674b4162fe39516081ae3479
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2024-09-05 17:05:38 +0200

    test: Expect two updating buttons

--- packages/app_center/test/manage_page_test.dart
+++ packages/app_center/test/manage_page_test.dart
@@ -301,7 +301,7 @@ void main() {
 
     final refreshButton =
         find.buttonWithText(tester.l10n.snapActionUpdatingLabel);
-    expect(refreshButton, findsOneWidget);
+    expect(refreshButton, findsExactly(2));
     expect(refreshButton, isDisabled);
 
     final testTile = find.snapTile('Snap with an update');
