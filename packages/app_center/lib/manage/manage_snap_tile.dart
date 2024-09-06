e4525c315f00c435e527e6773d972705446beb08
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2024-09-06 17:42:02 +0200

    refactor: Unify button bar

--- packages/app_center/lib/manage/manage_snap_tile.dart
+++ packages/app_center/lib/manage/manage_snap_tile.dart
@@ -3,6 +3,7 @@ import 'package:app_center/layout.dart';
 import 'package:app_center/manage/manage_l10n.dart';
 import 'package:app_center/snapd/snap_action.dart';
 import 'package:app_center/snapd/snapd.dart';
+import 'package:app_center/src/l10n/app_localizations.g.dart';
 import 'package:app_center/store/store.dart';
 import 'package:app_center/widgets/snap_menu_item.dart';
 import 'package:app_center/widgets/widgets.dart';
@@ -161,9 +162,7 @@ class ManageSnapTile extends StatelessWidget {
               ),
           ],
         ),
-        trailing: showUpdateButton
-            ? _ButtonBarForUpdate(snap)
-            : _ButtonBarForOpen(snap),
+        trailing: _ButtonBar(snap, showUpdateButton),
       ),
     );
   }
@@ -188,10 +187,11 @@ ManageTilePosition determineTilePosition({
   }
 }
 
-class _ButtonBarForUpdate extends ConsumerWidget {
-  const _ButtonBarForUpdate(this.snap);
+class _ButtonBar extends ConsumerWidget {
+  const _ButtonBar(this.snap, this.showUpdateButton);
 
   final Snap snap;
+  final bool showUpdateButton;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -200,143 +200,116 @@ class _ButtonBarForUpdate extends ConsumerWidget {
     final snapModel = ref.watch(snapModelProvider(snap.name));
     final activeChangeId = snapModel.valueOrNull?.activeChangeId;
     final removeColor = Theme.of(context).colorScheme.error;
+    final initialWidgets = _initialWidgetOrder(
+      snapModel: snapModel,
+      snapLauncher: snapLauncher,
+      l10n: l10n,
+      activeChangeId: activeChangeId,
+      showUpdateButton: showUpdateButton,
+    );
+    final primaryWidget = ConstrainedBox(
+      constraints: const BoxConstraints(minWidth: 100),
+      child: initialWidgets.firstOrNull ?? const SizedBox(),
+    );
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        primaryWidget,
+        const SizedBox(width: 16),
+        MenuAnchor(
+          menuChildren: [
+            ...initialWidgets.skip(1),
+            SnapMenuItem(
+              onPressed: () =>
+                  StoreNavigator.pushSnap(context, name: snap.name),
+              title: l10n.managePageShowDetailsLabel,
+            ),
+            SnapMenuItem(
+              onPressed: ref.read(snapModelProvider(snap.name).notifier).remove,
+              title: SnapAction.remove.label(l10n),
+              textStyle: TextStyle(color: removeColor),
+            ),
+          ],
+          builder: (context, controller, child) => YaruOptionButton(
+            onPressed: controller.isOpen ? controller.close : controller.open,
+            child: const Icon(YaruIcons.view_more_horizontal),
+          ),
+        ),
+      ],
+    );
+  }
+
+  List<Widget> _initialWidgetOrder({
+    required AsyncValue<SnapData> snapModel,
+    required AppLocalizations l10n,
+    required SnapLauncher snapLauncher,
+    required bool showUpdateButton,
+    required String? activeChangeId,
+  }) {
+    final hasActiveChange = activeChangeId != null;
+    final canOpen = snapLauncher.isLaunchable;
+    return [
+      if (hasActiveChange)
+        ActiveChangeContent(activeChangeId)
+      else ...[
+        if (showUpdateButton)
+          _UpdateButton(snapModel: snapModel, activeChangeId: activeChangeId),
+        if (!showUpdateButton && canOpen)
+          OutlinedButton(
+            onPressed: snapLauncher.open,
+            child: Text(l10n.snapActionOpenLabel),
+          ),
+        if (showUpdateButton && canOpen)
+          SnapMenuItem(
+            onPressed: snapLauncher.open,
+            title: l10n.snapActionOpenLabel,
+          ),
+      ],
+    ];
+  }
+}
+
+class _UpdateButton extends ConsumerWidget {
+  const _UpdateButton({
+    required this.snapModel,
+    required this.activeChangeId,
+  });
+
+  final AsyncValue<SnapData> snapModel;
+  final String? activeChangeId;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final l10n = AppLocalizations.of(context);
     final shouldQuitToUpdate =
         snapModel.valueOrNull?.localSnap?.refreshInhibit != null;
+    final snap =
+        snapModel.valueOrNull?.localSnap ?? snapModel.valueOrNull?.storeSnap;
 
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        if (shouldQuitToUpdate)
-          const QuitToUpdateNotice()
-        else
-          OutlinedButton(
-            onPressed: activeChangeId != null || !snapModel.hasValue
-                ? null
-                : ref.read(snapModelProvider(snap.name).notifier).refresh,
-            child: activeChangeId != null
-                ? ActiveChangeContent(activeChangeId)
-                : const _UpdateButton(),
-          ),
-        const SizedBox(width: 16),
-        MenuAnchor(
-          menuChildren: [
-            if (snapLauncher.isLaunchable)
-              SnapMenuItem(
-                onPressed: snapLauncher.open,
-                title: l10n.snapActionOpenLabel,
+    if (shouldQuitToUpdate) {
+      return const QuitToUpdateNotice();
+    } else {
+      return OutlinedButton(
+        onPressed: activeChangeId != null || !snapModel.hasValue
+            ? null
+            : ref.read(snapModelProvider(snap!.name).notifier).refresh,
+        child: activeChangeId != null
+            ? ActiveChangeContent(activeChangeId!)
+            : Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Icon(YaruIcons.download),
+                  const SizedBox(width: kSpacingSmall),
+                  Text(
+                    l10n.snapActionUpdateLabel,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
               ),
-            SnapMenuItem(
-              onPressed: () =>
-                  StoreNavigator.pushSnap(context, name: snap.name),
-              title: l10n.managePageShowDetailsLabel,
-            ),
-            SnapMenuItem(
-              onPressed: ref.read(snapModelProvider(snap.name).notifier).remove,
-              title: SnapAction.remove.label(l10n),
-              textStyle: TextStyle(color: removeColor),
-            ),
-          ],
-          builder: (context, controller, child) => YaruOptionButton(
-            onPressed: () {
-              if (controller.isOpen) {
-                controller.close();
-              } else {
-                controller.open();
-              }
-            },
-            child: const Icon(YaruIcons.view_more_horizontal),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class _ButtonBarForOpen extends ConsumerWidget {
-  const _ButtonBarForOpen(this.snap);
-
-  final Snap snap;
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final l10n = AppLocalizations.of(context);
-    final snapLauncher = ref.watch(launchProvider(snap));
-    final snapModel = ref.watch(snapModelProvider(snap.name));
-    final activeChangeId = snapModel.valueOrNull?.activeChangeId;
-    final removeColor = Theme.of(context).colorScheme.error;
-
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Stack(
-          children: [
-            if (activeChangeId != null)
-              OutlinedButton(
-                onPressed: null,
-                child: ActiveChangeContent(activeChangeId),
-              ),
-            Visibility(
-              maintainSize: true,
-              maintainAnimation: true,
-              maintainState: true,
-              visible: snapLauncher.isLaunchable && activeChangeId == null,
-              child: OutlinedButton(
-                onPressed: snapLauncher.open,
-                child: Text(
-                  l10n.snapActionOpenLabel,
-                ),
-              ),
-            ),
-          ],
-        ),
-        const SizedBox(width: 16),
-        MenuAnchor(
-          menuChildren: [
-            SnapMenuItem(
-              onPressed: () =>
-                  StoreNavigator.pushSnap(context, name: snap.name),
-              title: l10n.managePageShowDetailsLabel,
-            ),
-            SnapMenuItem(
-              onPressed: ref.read(snapModelProvider(snap.name).notifier).remove,
-              title: SnapAction.remove.label(l10n),
-              textStyle: TextStyle(color: removeColor),
-            ),
-          ],
-          builder: (context, controller, child) => YaruOptionButton(
-            onPressed: () {
-              if (controller.isOpen) {
-                controller.close();
-              } else {
-                controller.open();
-              }
-            },
-            child: const Icon(YaruIcons.view_more_horizontal),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class _UpdateButton extends StatelessWidget {
-  const _UpdateButton();
-
-  @override
-  Widget build(BuildContext context) {
-    final l10n = AppLocalizations.of(context);
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        const Icon(YaruIcons.download),
-        const SizedBox(width: 8),
-        Text(
-          l10n.snapActionUpdateLabel,
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-        ),
-      ],
-    );
+      );
+    }
   }
 }
 
