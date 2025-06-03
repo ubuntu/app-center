ae788ef3fe026f607c0f5f266bf8c6ecd59c33b6
ashuntu <ashton.nelson@canonical.com>
2025-06-03 16:50:45 -0500

    Add accessible label to expandable page sections

--- packages/app_center/lib/widgets/app_page_section.dart
+++ packages/app_center/lib/widgets/app_page_section.dart
@@ -1,10 +1,42 @@
+import 'package:app_center/l10n.dart';
+import 'package:flutter/widgets.dart';
 import 'package:yaru/yaru.dart';
 
-class AppPageSection extends YaruExpandable {
-  const AppPageSection({required super.header, required super.child, super.key})
-      : super(
-          expandButtonPosition: YaruExpandableButtonPosition.start,
-          isExpanded: true,
-          gapHeight: 24,
-        );
+class AppPageSection extends StatefulWidget {
+  const AppPageSection({
+    required this.header,
+    required this.child,
+    super.key,
+  });
+
+  final Widget header;
+  final Widget child;
+
+  @override
+  State<AppPageSection> createState() => _AppPageSectionState();
+}
+
+class _AppPageSectionState extends State<AppPageSection> {
+  bool expanded = true;
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context);
+
+    return YaruExpandable(
+      expandButtonPosition: YaruExpandableButtonPosition.start,
+      gapHeight: 24,
+      isExpanded: expanded,
+      header: widget.header,
+      expandIcon: Icon(
+        YaruIcons.pan_end,
+        semanticLabel:
+            expanded ? l10n.sectionCollapseLabel : l10n.sectionExpandLabel,
+      ),
+      onChange: (expanded) => setState(() {
+        this.expanded = expanded;
+      }),
+      child: widget.child,
+    );
+  }
 }
