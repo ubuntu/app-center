ef4215c4a1c0ac47766fc16bb374752d5f5bc2c1
ashuntu <ashton.nelson@canonical.com>
2026-03-05 19:37:46 -0600

    feat: Add unified AppTitleBar for app titles

---
+++ packages/app_center/lib/apps/app_title_bar.dart
@@ -0,0 +1,102 @@
+import 'package:app_center/appstream/appstream.dart';
+import 'package:app_center/deb/deb_model.dart';
+import 'package:app_center/deb/local_deb_model.dart';
+import 'package:app_center/layout.dart';
+import 'package:app_center/snapd/snapd.dart';
+import 'package:app_center/widgets/app_icon.dart';
+import 'package:app_center/widgets/app_title.dart';
+import 'package:flutter/material.dart';
+
+/// Title bar for an app. Usually with an app icon, title, and action buttons.
+class AppTitleBar extends StatelessWidget {
+  const AppTitleBar({
+    required this.title,
+    this.banner,
+    this.actions,
+    this.iconUrl,
+    super.key,
+  });
+
+  factory AppTitleBar.fromSnap(
+    SnapData snapData, {
+    Widget? actions,
+    Widget? banner,
+  }) =>
+      AppTitleBar(
+        iconUrl: snapData.snap.iconUrl,
+        title: AppTitle.fromSnap(
+          snapData.snap,
+          large: true,
+        ),
+        actions: actions,
+        banner: banner,
+      );
+
+  factory AppTitleBar.fromDeb(
+    DebData debData, {
+    Widget? actions,
+    Widget? banner,
+  }) =>
+      AppTitleBar(
+        iconUrl: debData.component.icon,
+        title: AppTitle.fromDeb(
+          debData.component,
+          large: true,
+        ),
+        actions: actions,
+        banner: banner,
+      );
+
+  factory AppTitleBar.fromLocalDeb(
+    LocalDebData localDebData, {
+    Widget? actions,
+    Widget? banner,
+  }) =>
+      AppTitleBar(
+        title: AppTitle.fromLocalDeb(
+          localDebData,
+          large: true,
+        ),
+        actions: actions,
+        banner: banner,
+      );
+
+  /// [AppTitle] widget for the app.
+  final Widget title;
+
+  /// Optional banner to display below the app title.
+  final Widget? banner;
+
+  /// Optional widget for app actions like report and share buttons.
+  final Widget? actions;
+
+  /// Optional URL to use for the app's icon.
+  final String? iconUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AppIcon(iconUrl: iconUrl, size: 96),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Semantics(
+                header: true,
+                focused: true,
+                child: title,
+              ),
+            ),
+            if (actions != null) actions!,
+          ],
+        ),
+        if (banner != null) ...[
+          const SizedBox(height: kPagePadding),
+          banner!,
+        ],
+      ],
+    );
+  }
+}
