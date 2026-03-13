7603ec535645ad44cd14dae1ad0df9a4ab3d0635
ivo <ivoheck99@gmail.com>
2026-03-13 20:32:18 +0100

    removing old code

--- packages/app_center/lib/providers/installed_snaps_provider.dart
+++ packages/app_center/lib/providers/installed_snaps_provider.dart
@@ -15,18 +15,28 @@ class InstalledSnaps extends _$InstalledSnaps {
     return connectionCheck(_snapd.getSnaps, ref);
   }
 
+  // Used to add a snap from the list without reloading the whole provider.
+  // Should be used when a snap is uninstalled directly from the manage page
+  // list for example.
   Future<void> addToList(Snap snap) async {
     if (!state.hasValue) return;
     final localSnap = await _snapd.getSnap(snap.name);
-    state = AsyncData(state.value!.copyWith(
-      snaps: [...state.value!.snaps, localSnap],
-    ));
+    state = AsyncData(
+      state.value!.copyWith(
+        snaps: [...state.value!.snaps, localSnap],
+      ),
+    );
   }
 
+  // Used to remove a snap from the list without reloading the whole provider.
+  // Should be used when a snap is uninstalled directly from the manage page
+  // list for example.
   void removeFromList(String snapName) {
     if (!state.hasValue) return;
-    state = AsyncData(state.value!.copyWith(
-      snaps: state.value!.snaps.where((s) => s.name != snapName),
-    ));
+    state = AsyncData(
+      state.value!.copyWith(
+        snaps: state.value!.snaps.where((s) => s.name != snapName),
+      ),
+    );
   }
 }
