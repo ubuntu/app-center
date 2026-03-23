a5ee40f889728dbf2856c3d65b6b9e77bf4b875e
Gaurav Saini <me@gauravsaini.com>
2026-03-24 08:37:38 +1100

    fix(a11y): make links focusable with keyboard (#1591)
    Remove canRequestFocus:false from SelectionArea in app info bar
    so that hyperlinks can receive keyboard focus and be accessible
    to screen readers.

--- packages/app_center/lib/widgets/app_info_bar.dart
+++ packages/app_center/lib/widgets/app_info_bar.dart
@@ -246,7 +246,6 @@ class _AppInfoItem extends StatelessWidget {
         children: [
           label,
           SelectionArea(
-            focusNode: FocusNode(canRequestFocus: false),
             child: DefaultTextStyle.merge(
               style: const TextStyle(fontWeight: FontWeight.w500),
               child: value,
