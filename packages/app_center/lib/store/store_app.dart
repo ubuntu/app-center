c3747402aa795e967ea58d0abe31f378cdcd0b66
ashuntu <ashton.nelson@canonical.com>
2026-09-02 17:09:11 -0500

    fix: Remove search bar from Add-ons and Drivers pages

--- packages/app_center/lib/store/store_app.dart
+++ packages/app_center/lib/store/store_app.dart
@@ -223,7 +223,11 @@ class _StoreAppHome extends ConsumerWidget {
           StoreRoutes.additionalDrivers => MaterialPageRoute(
             settings: settings,
             builder: (_) => YaruDetailPage(
-              appBar: searchField,
+              appBar: YaruWindowTitleBar(
+                border: BorderSide.none,
+                leading: _MaybeBackButton(navigatorKey),
+                title: Text(AdditionalDriversPage.label(context)),
+              ),
               body: const AdditionalDriversPage(),
             ),
           ),
